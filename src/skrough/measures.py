5427a68999a42b829e923aa81064955f23ba2878
sebov <12091011+sebov@users.noreply.github.com>
2022-01-08 16:13:44 +0100

    build: Setup project (#4)
    * Add chaos measures
    
    * Add package features
    
    * Add package features
    
    * Update gitignore
    
    * Add tests
    
    * build: Add jupyter-book dev dependency
    
    * docs: Init docs (jupyter-book)
    
    * build: Add pytest-cov dev dependency
    
    * docs: Update readme file
    
    * build: Add pre-commit dev dependency
    
    * build: Add python-semantic-release as dev dependency
    
    * build: Add tox dev dependency
    
    * build: Add pre-commit for exporting Sphinx configuration
    
    * build: Add empty Sphinx configuration
    
    * build: Update docs configuration
    
    * docs: Add pre-commit badge
    
    * build: Add pre-commit hooks - black, flake8
    
    * docs: Fix docs end of line
    
    * build: Add pre-commit hooks, fix code formatting
    
    * build: Add pre-commit - mypy
    
    * build: Update pre-commit hooks
    
    * build: Add pre-commit hook - pyupgrade
    
    * build: Add pre-commit hooks - nbqa
    
    * docs: Add isort, codecov badges
    
    * ci: Configure tests with github actions
    
    * ci: Configure sending coverage report to codecov
    
    * ci: Add documentation build check
    
    * ci: Setup CD - package build and publish (Test PyPI only)
    
    * ci: Test build and publish from dev3 branch
    
    * ci: Disable CD job, restore package start version
    
    * build: Change semantic-release config
    
    * build: Add pre-commit hooks - black, pylint, pydocstyle

---
+++ src/skrough/measures.py
@@ -0,0 +1,52 @@
+import numba
+import numpy as np
+
+
+@numba.njit
+def gini_impurity(distribution: np.ndarray, n: int):
+    """
+    Compute average gini impurity
+
+    Compute average gini impurity using the following formula
+
+    .. math:: \\sum((1 - \\sum(counts^2)/(\\sum(counts)^2)) * \\sum(counts)) / n
+
+    where counts correspond to rows in distribution
+    """
+    nrow = distribution.shape[0]
+    ncol = distribution.shape[1]
+    result = 0.0
+    for i in numba.prange(nrow):
+        group_count = 0
+        sum_squared_counts = 0
+        for j in range(ncol):
+            x = distribution[i, j]
+            group_count += x
+            sum_squared_counts += x * x
+        if group_count > 0:
+            result += (1.0 - sum_squared_counts / (group_count * group_count)) * (
+                group_count / n
+            )
+    return result
+
+
+@numba.njit
+def entropy(distribution: np.ndarray, n: int):
+    """
+    Compute average entropy
+    """
+    nrow = distribution.shape[0]
+    ncol = distribution.shape[1]
+    result = 0.0
+    for i in numba.prange(nrow):
+        group_count = 0
+        for j in range(ncol):
+            group_count += distribution[i, j]
+        if group_count > 0:
+            tmp = 0.0
+            for j in range(ncol):
+                if distribution[i, j] > 0:
+                    p = distribution[i, j] / group_count
+                    tmp -= p * np.log2(p)
+            result += tmp * (group_count / n)
+    return result
