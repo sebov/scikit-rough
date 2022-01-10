7aede68958469db9a6ca76dee5055557aaa0e09d
sebov <12091011+sebov@users.noreply.github.com>
2022-01-10 23:41:58 +0100

    tests: Add test to increase coverage - dataprep

--- src/skrough/__init__.py
+++ src/skrough/__init__.py
@@ -1,1 +1,10 @@
+"""A rough sets toolkit for Python.
+
+skrough is a Python module that provides low level rough sets method for
+experimenting with data
+"""
+
+from . import dataprep
+
+__all__ = ["dataprep"]
 __version__ = "0.1.0"
