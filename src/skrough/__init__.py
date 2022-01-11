e9c9c08987a9df83766b97b65b22c524f17874be
sebov <12091011+sebov@users.noreply.github.com>
2022-01-12 00:16:16 +0100

    test: Add tests to increase coverage - group_index

--- src/skrough/__init__.py
+++ src/skrough/__init__.py
@@ -4,7 +4,12 @@ skrough is a Python module that provides low level rough sets method for
 experimenting with data
 """
 
-from . import chaos_score, dataprep, measures
+from . import chaos_score, dataprep, group_index, measures
 
-__all__ = ["chaos_score", "dataprep", "measures"]
+__all__ = [
+    "chaos_score",
+    "dataprep",
+    "group_index",
+    "measures",
+]
 __version__ = "0.1.0"
