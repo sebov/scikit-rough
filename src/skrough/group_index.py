642fa5cb5d776c7e0555a943b376f75a01a458eb
sebov <12091011+sebov@users.noreply.github.com>
2022-01-12 11:30:33 +0100

    feat: Add data preparation function
    Also refactor other code

--- src/skrough/group_index.py
+++ src/skrough/group_index.py
@@ -25,21 +25,21 @@ def split_groups(
 
 
 # TODO: check if it its actually slower
-def batch_split_into_groups_slower(xx, xx_count_distinct, attrs):
-    """
-    Split objects into groups according to values on given attributes
-    """
-    group_index = np.zeros(len(xx), dtype=np.int_)
-    n_groups = 1
-    for attr in attrs:
-        group_index, n_groups = split_groups(
-            group_index,
-            n_groups,
-            xx[:, attr],
-            xx_count_distinct[attr],
-            compress_group_index=True,
-        )
-    return group_index, n_groups
+# def batch_split_into_groups_slower(xx, xx_count_distinct, attrs):
+#     """
+#     Split objects into groups according to values on given attributes
+#     """
+#     group_index = np.zeros(len(xx), dtype=np.int_)
+#     n_groups = 1
+#     for attr in attrs:
+#         group_index, n_groups = split_groups(
+#             group_index,
+#             n_groups,
+#             xx[:, attr],
+#             xx_count_distinct[attr],
+#             compress_group_index=True,
+#         )
+#     return group_index, n_groups
 
 
 def batch_split_into_groups(xx, xx_count_distinct, attrs):
