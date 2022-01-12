2051cdb78b68968912f0894ab8adfb7df8ee5f0d
sebov <12091011+sebov@users.noreply.github.com>
2022-01-12 02:30:24 +0100

    test: Add tests to increase coverage - feature_importance

--- src/skrough/__init__.py
+++ src/skrough/__init__.py
@@ -4,11 +4,12 @@ skrough is a Python module that provides low level rough sets method for
 experimenting with data
 """
 
-from . import chaos_score, dataprep, group_index, measures
+from . import chaos_score, dataprep, feature_importance, group_index, measures
 
 __all__ = [
     "chaos_score",
     "dataprep",
+    "feature_importance",
     "group_index",
     "measures",
 ]
