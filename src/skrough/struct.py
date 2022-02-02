5e0fa49fc64189a7df47a236930195397436b21c
sebov <12091011+sebov@users.noreply.github.com>
2022-02-02 01:03:50 +0100

    Add GroupIndex

---
+++ src/skrough/struct.py
@@ -0,0 +1,35 @@
+import numpy as np
+from attrs import define
+
+
+@define
+class GroupIndex:
+
+    index: np.ndarray
+    count: np.int64
+
+    @classmethod
+    def get_empty(cls):
+        return cls(
+            index=np.empty(0, dtype=np.int64),
+            count=np.int64(0),
+        )
+
+    @classmethod
+    def get_one_group(cls, size):
+        return cls(
+            index=np.zeros(size, dtype=np.int64),
+            count=np.int64(1),
+        )
+
+
+@define
+class Reduct:
+
+    attrs: list[int]
+
+
+@define
+class Bireduct(Reduct):
+
+    objects: list[int]
