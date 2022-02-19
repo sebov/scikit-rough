ac93b4bd0beab2653601da19cf802c5a8c66fdea
sebov <12091011+sebov@users.noreply.github.com>
2022-02-19 16:17:32 +0100

    Remove functional dependency check slow function

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,9 +1,6 @@
 import pytest
 
-from skrough.checks import (
-    check_functional_dependency,
-    check_functional_dependency_slower,
-)
+from skrough.checks import check_functional_dependency
 
 
 @pytest.mark.parametrize(
@@ -29,33 +26,6 @@ from skrough.checks import (
 def test_checks_functional_dependency(objs, attrs, expected_result, golf_dataset_prep):
     x, _, y, _ = golf_dataset_prep
     assert check_functional_dependency(x, y, objs, attrs) == expected_result
-
-
-@pytest.mark.parametrize(
-    "objs, attrs, expected_result",
-    [
-        (range(14), range(4), True),
-        (None, None, True),
-        ([], [], True),
-        ([], None, True),
-        ([], [0, 1], True),
-        ([1, 2], None, True),
-        ([1, 2], [1, 2], False),
-        ([0, 1, 5, 7, 13], [], True),
-        ([2, 3, 4, 6, 8, 9, 10, 11, 12], [], True),
-        (None, [0], False),
-        (None, [0, 1], False),
-        (None, [0, 1, 2], False),
-        (None, [0, 1, 3], True),
-        (None, [0, 2, 3], True),
-        (None, [1, 2, 3], False),
-    ],
-)
-def test_checks_functional_dependency_slower(
-    objs, attrs, expected_result, golf_dataset_prep
-):
-    x, _, y, _ = golf_dataset_prep
-    assert check_functional_dependency_slower(x, y, objs, attrs) == expected_result
 
 
 def test_checks_reduct(golf_dataset_prep):
