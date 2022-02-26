dbc6383132640dc62be9218abf47e10ed2f169a8
sebov <12091011+sebov@users.noreply.github.com>
2022-02-26 14:44:37 +0100

    Refactor choose object function

--- src/skrough/instances.py
+++ src/skrough/instances.py
@@ -1,45 +1,25 @@
 from typing import Optional
 
+import numba
 import numpy as np
-import numpy.typing as npt
 
 import skrough as rgh
 import skrough.typing as rght
-from skrough.containers import GroupIndex
 
 
-def choose_objects_slower(
-    group_index: GroupIndex,
-    y: np.ndarray,
-    permutation: Optional[npt.ArrayLike] = None,
-    seed: rght.RandomState = None,
-):
-    """
-    Choose objects having uniform decision values within their groups
-    """
-    if permutation is None:
-        rng = np.random.default_rng(seed)
-        permutation = rng.permutation(len(group_index.index))
-    else:
-        permutation = np.array(permutation)
-    _, idx = np.unique(group_index.index[permutation], return_index=True)
-    idx = permutation[idx]
-    group_dec_values = dict(zip(group_index.index[idx], y[idx]))
-    result = [
-        i
-        for i in range(len(group_index.index))
-        if y[i] == group_dec_values[group_index.index[i]]
-    ]
-    return result
+@numba.njit
+def get_pos_where_values_in(values, reference):
+    reference = set(reference)
+    return [i for i in range(len(values)) if values[i] in reference]
 
 
 def choose_objects(
-    group_index,
-    dec_values,
-    dec_values_count,
-    objs=None,
-    weights=None,
-    seed=None,
+    group_index: rgh.containers.GroupIndex,
+    y: np.ndarray,
+    y_count: int,
+    objs: Optional[np.ndarray] = None,
+    weights: Optional[np.ndarray] = None,
+    seed: rght.RandomState = None,
 ):
     """
     Choose objects having uniform decision values within their groups
@@ -52,31 +32,31 @@ def choose_objects(
     if len(group_index.index) == 0:
         return []
 
-    def normalize(weights):
-        norm = np.linalg.norm(weights, ord=1)
+    def normalize(values: np.ndarray) -> np.ndarray:
+        norm = np.linalg.norm(values, ord=1)
         if norm > 0:
-            weights = weights / norm
-        return weights
+            values = values / norm
+        return values
 
+    proba = None
     if weights is not None:
-        weights = np.asarray(weights)
-        weights = normalize(weights)
-        if any(weights == 0):
-            weights += np.finfo(dtype=np.float64).eps
-            weights = normalize(weights)
-        print(weights)
+        proba = np.asarray(proba)
+        proba = normalize(proba)
+        if any(proba == 0):
+            proba += np.finfo(dtype=np.float64).eps
+            proba = normalize(proba)
 
     if objs is None:
         rng = np.random.default_rng(seed)
         n = len(group_index.index)
-        selector = rng.choice(n, size=n, replace=False, p=weights)
+        selector = rng.choice(n, size=n, replace=False, p=proba)
     else:
         selector = np.asarray(objs)
 
     _, idx = np.unique(group_index.index[selector], return_index=True)
     idx = selector[idx]
-    tmp = rgh.group_index.split_groups(
-        group_index, dec_values, dec_values_count, compress_group_index=False
+    group_index_dec = rgh.group_index.split_groups(
+        group_index, y, y_count, compress_group_index=False
     )
-    tmp2 = tmp.index[idx]
-    return np.arange(len(tmp.index))[np.isin(tmp.index, tmp2)].tolist()
+    chosen = group_index_dec.index[idx]
+    return get_pos_where_values_in(values=group_index_dec.index, reference=chosen)
