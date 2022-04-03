9fb384145a0d7a7b82bc10f2a67389eb1637c02c
sebov <12091011+sebov@users.noreply.github.com>
2022-04-03 17:31:15 +0200

    Add init and stop hooks

--- src/skrough/typing.py
+++ src/skrough/typing.py
@@ -5,10 +5,19 @@ import numpy as np
 from skrough.structs.reduct import (  # noqa: F401 # pylint: disable=unused-import
     ReductLike,
 )
+from skrough.structs.state import GrowShrinkState
 
 ChaosMeasureReturnType = float
 ChaosMeasure = Callable[[np.ndarray, int], ChaosMeasureReturnType]
 
 Seed = Optional[Union[int, np.random.SeedSequence, np.random.Generator]]
 
-InitStateHook = Callable[[np.ndarray, np.ndarray, np.ndarray, int], None]
+GSInitStateHook = Callable[
+    [np.ndarray, np.ndarray, np.ndarray, int, GrowShrinkState],
+    None,
+]
+
+GSStopHook = Callable[
+    [np.ndarray, np.ndarray, np.ndarray, int, GrowShrinkState],
+    bool,
+]
