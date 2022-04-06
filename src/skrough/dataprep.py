6762947c728c009792da469c5cdbcd937dd82c84
sebov <12091011+sebov@users.noreply.github.com>
2022-04-07 00:38:51 +0200

    Add shadow attrs generator

--- src/skrough/dataprep.py
+++ src/skrough/dataprep.py
@@ -10,9 +10,11 @@ import numpy as np
 import pandas as pd
 import sklearn.utils
 
+import skrough.typing as rght
 
-def _prepare_values(values):
-    """Factorize values."""
+
+def _prepare_values(values: np.ndarray):
+    """Prepare enumerated values along with a number of distinct values."""
     factorized_values, uniques = pd.factorize(values, na_sentinel=None)  # type: ignore
     count_distinct = len(uniques)
     return factorized_values, count_distinct
@@ -49,3 +51,19 @@ def prepare_factorized_data(
     y, y_count_distinct = _prepare_values(data_y)
     x, y = sklearn.utils.check_X_y(x, y, multi_output=False)
     return x, x_count_distinct, y, y_count_distinct
+
+
+def add_shadow_attrs(
+    df: pd.DataFrame,
+    target_column: Union[str, int],
+    shadow_attrs_prefix: str,
+    seed: rght.Seed = None,
+) -> pd.DataFrame:
+    rng = np.random.default_rng(seed)
+    data_y = df[target_column]
+    data_x = df.drop(columns=target_column)
+    data_x_shadow = data_x.apply(lambda col: rng.permutation(col))
+    col_names = list(shadow_attrs_prefix + data_x_shadow.columns.astype(str))
+    data_x_shadow.columns = col_names
+    result = pd.concat([data_x, data_x_shadow, data_y], axis=1)
+    return result
