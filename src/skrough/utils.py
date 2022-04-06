b2079dfc0a1cf01fe8789f9d36faa8279616f1dd
sebov <12091011+sebov@users.noreply.github.com>
2022-04-06 14:07:45 +0200

    Refactor minmax

--- src/skrough/utils.py
+++ src/skrough/utils.py
@@ -8,7 +8,7 @@ NAN_VALUES_PRESENT_MSG = "There is a nan value present in the array"
 
 
 @numba.njit
-def minmax(ar: np.ndarray) -> Tuple[Any, Any]:
+def minmax(values: np.ndarray) -> Tuple[Any, Any]:
     """Returns min and max values.
 
     Returns min and max values from a given array. Checks if the array is not empty
@@ -23,15 +23,15 @@ def minmax(ar: np.ndarray) -> Tuple[Any, Any]:
     Returns:
         A pair of min and max values from the input array.
     """
-    length = len(ar)
+    length = len(values)
     if length == 0:
         raise ValueError(EMPTY_ARRAY_MSG)
-    if np.isnan(ar[0]):
+    if np.isnan(values[0]):
         raise ValueError(NAN_VALUES_PRESENT_MSG)
-    _min = _max = ar[0]
+    _min = _max = values[0]
     for i in range(1, length):
-        if np.isnan(ar[i]):
+        if np.isnan(values[i]):
             raise ValueError(NAN_VALUES_PRESENT_MSG)
-        _min = min(_min, ar[i])
-        _max = max(_max, ar[i])
+        _min = min(_min, values[i])
+        _max = max(_max, values[i])
     return _min, _max
