84d27e1650faffc884a6888a5f53a62850c72b18
sebov <12091011+sebov@users.noreply.github.com>
2022-04-06 14:29:18 +0200

    Add get_uniques_index docs and tests

--- src/skrough/unique.py
+++ src/skrough/unique.py
@@ -2,5 +2,27 @@ import numpy as np
 
 
 def get_uniques_index(values: np.ndarray) -> np.ndarray:
+    """Get positions of first occurrences of unique values.
+
+    Get positions in the input array for which the given unique values appeared
+    for the first time. Unique values are considered in the ascending order of their
+    values, i.e., from the lowest to the highest unique values occuring in the input
+    array, positions of their first occurrence is returned.
+
+    Args:
+        values: Input array.
+
+    Returns:
+        The positions in the input array for which given values appeared for
+        the first time.
+
+    Examples:
+        >>> get_uniques_index(np.array([1, 2, 3]))
+        [0, 1, 2]
+        >>> get_uniques_index(np.array([3, 2, 1]))
+        [2, 1, 0]
+        >>> get_uniques_index(np.array([1, 1, 1]))
+        [0]
+    """
     _, idx = np.unique(values, return_index=True)
     return idx
---
+++ tests/test_unique.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+
+from skrough.unique import get_uniques_index
+
+
+@pytest.mark.parametrize(
+    "values, expected",
+    [
+        ([1, 2, 3, 4], [0, 1, 2, 3]),
+        ([4, 3, 2, 1], [3, 2, 1, 0]),
+        ([1, 1, 1, 1], [0]),
+        ([], []),
+        ([1, 2, 1, 2, 1, 2], [0, 1]),
+    ],
+)
+def test_get_uniques_index(values, expected):
+    values = np.asarray(values)
+    expected = np.asarray(expected)
+    assert np.array_equal(get_uniques_index(values), expected)
