97a0568d405777bf56557069d91586d14a0ac354
sebov <12091011+sebov@users.noreply.github.com>
2022-04-06 14:13:16 +0200

    Refactor test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from skrough.utils import minmax
 
 
 @pytest.mark.parametrize(
-    "vals, expected_min_max",
+    "values, expected_min_max",
     [
         ([1, 2, 3], (1, 3)),
         ([3, 1, 1, 1, -1, 1], (-1, 3)),
@@ -16,18 +16,18 @@ from skrough.utils import minmax
         ([-np.inf, 1, np.inf], (-np.inf, np.inf)),
     ],
 )
-def test_minmax(vals, expected_min_max):
-    assert minmax(np.asarray(vals)) == expected_min_max
+def test_minmax(values, expected_min_max):
+    assert minmax(np.asarray(values)) == expected_min_max
 
 
 @pytest.mark.parametrize(
-    "vals",
+    "values",
     [
         [],
         [np.inf, np.nan],
         [1, 1, 1, np.nan],
     ],
 )
-def test_minmax_raise(vals):
+def test_minmax_raise(values):
     with pytest.raises(ValueError):
-        minmax(np.asarray(vals))
+        minmax(np.asarray(values))
