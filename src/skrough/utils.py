91a46b4e7973d8e2ce7dc7dec509fef615e38876
sebov <12091011+sebov@users.noreply.github.com>
2022-04-06 13:55:27 +0200

    Add tests for minmax function

--- src/skrough/utils.py
+++ src/skrough/utils.py
@@ -1,14 +1,37 @@
+from typing import Any, Tuple
+
 import numba
 import numpy as np
 
+EMPTY_ARRAY_MSG = "Empty array specified"
+NAN_VALUES_PRESENT_MSG = "There is a nan value present in the array"
+
 
 @numba.njit
-def minmax(ar: np.ndarray):
+def minmax(ar: np.ndarray) -> Tuple[Any, Any]:
+    """Returns min and max values.
+
+    Returns min and max values from a given array. Checks if the array is not empty
+    and does not contain ``nan`` values.
+
+    Args:
+        ar: Input array.
+
+    Raises:
+        ValueError: When the input array is empty or contains nan values.
+
+    Returns:
+        A pair of min and max values from the input array.
+    """
     length = len(ar)
     if length == 0:
-        raise ValueError("empty array specified")
+        raise ValueError(EMPTY_ARRAY_MSG)
+    if np.isnan(ar[0]):
+        raise ValueError(NAN_VALUES_PRESENT_MSG)
     _min = _max = ar[0]
     for i in range(1, length):
-        _min = min(ar[i], _min)
-        _max = max(ar[i], _max)
+        if np.isnan(ar[i]):
+            raise ValueError(NAN_VALUES_PRESENT_MSG)
+        _min = min(_min, ar[i])
+        _max = max(_max, ar[i])
     return _min, _max
