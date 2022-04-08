20e4ee06f620292e100225af9a3ddc12f9f4da56
sebov <12091011+sebov@users.noreply.github.com>
2022-04-08 23:19:35 +0200

    Remove old code

--- src/skrough/tmp.py
+++ src/skrough/tmp.py
@@ -1,7 +1,7 @@
-from typing import List
+# from typing import List
 
 # import skrough.typing as rght
-from skrough.chaos_score import get_chaos_score
+# from skrough.chaos_score import get_chaos_score
 
 # import numpy as np
 
@@ -24,38 +24,3 @@ from skrough.chaos_score import get_chaos_score
 #     distribution = compute_dec_distribution(group_index, n_groups,
 #                                             y, y_count_distinct)
 #     groups_homogeneity = compute_homogeneity(distribution)
-
-
-# def split_groups_and_compute_chaos_score_2(
-#     self, group_index, n_groups, attr_values, attr_count, y, y_count, chaos_fun
-# ):
-#     tmp_group_index, tmp_n_groups = rgh.group_index.split_groups(
-#         group_index, n_groups, attr_values, attr_count
-#     )
-#     return rgh.chaos_score.compute_chaos_score_for_group_index(
-#         tmp_group_index, tmp_n_groups, len(attr_values), y, y_count, chaos_fun
-#     )
-
-
-def reduction_phase(
-    xx,
-    xx_count_distinct,
-    yy,
-    yy_count_distinct,
-    # group_index,
-    # n_groups,
-    chaos_fun,
-    attrs: List[int],
-) -> List[int]:
-    before_reduction_chaos_score = get_chaos_score(
-        xx, xx_count_distinct, yy, yy_count_distinct, attrs, chaos_fun
-    )
-    result_attrs_reduction = set(attrs)
-    for i in reversed(attrs):
-        attrs_to_try = result_attrs_reduction - {i}
-        current_chaos_score = get_chaos_score(
-            xx, xx_count_distinct, yy, yy_count_distinct, list(attrs_to_try), chaos_fun
-        )
-        if current_chaos_score <= before_reduction_chaos_score:
-            result_attrs_reduction = attrs_to_try
-    return sorted(result_attrs_reduction)
