1da749c993d723c50ca3d3245896d035de2209cb
sebov <12091011+sebov@users.noreply.github.com>
2022-05-03 13:07:43 +0200

    Add docs to stop hooks

--- src/skrough/algorithms/hooks/stop_hooks.py
+++ src/skrough/algorithms/hooks/stop_hooks.py
@@ -22,6 +22,23 @@ logger = logging.getLogger(__name__)
 def stop_hook_approx_threshold(
     state: ProcessingState,
 ) -> bool:
+    """Stop check based on an expected level of approximation.
+
+    Stop check based on an expected level of approximation. The function implements a
+    check whether the current chaos score computed using the contents of the `config`
+    and `values` containers stored in the computation's state reached the expected
+    approximation level maintained in the state object's values by some other
+    complementary hook(s) under the `HOOKS_APPROX_CHAOS_SCORE_VALUE_THRESHOLD` key
+    constant. The stop check uses the values stored under the following key constants to
+    compute the current chaos score: `HOOKS_CHAOS_FUN` (`config`), `HOOKS_GROUP_INDEX`
+    (`values`), `HOOKS_DATA_Y` (`values`), `HOOKS_DATA_Y_COUNT` (`values`)).
+
+    Args:
+        state: State object that holds a computation's state.
+
+    Returns:
+        Indication whether the computation should stop.
+    """
     current_chaos_score = get_chaos_score_for_group_index(
         state.values[HOOKS_GROUP_INDEX],
         state.values[HOOKS_DATA_Y],
@@ -42,6 +59,20 @@ def stop_hook_approx_threshold(
 def stop_hook_attrs_count(
     state: ProcessingState,
 ) -> bool:
+    """Stop check based on a number of result attrs.
+
+    Stop check based on a number of result attrs. The function implements a simple check
+    whether the length of the result attrs collection (stored under the
+    `HOOKS_RESULT_ATTRS` key constant) in the state object's values reached the limit
+    setting (stored under the `HOOKS_RESULT_ATTRS_MAX_COUNT` key constant) from the
+    state object's config.
+
+    Args:
+        state: State object that holds a computation's state.
+
+    Returns:
+        Indication whether the computation should stop.
+    """
     return (
         len(state.values[HOOKS_RESULT_ATTRS])
         >= state.config[HOOKS_RESULT_ATTRS_MAX_COUNT]
@@ -55,9 +86,13 @@ def stop_hook_empty_iterations(
     """Stop check based on a number of empty iterations.
 
     Stop check based on a number of empty iterations. The function implements a simple
-    check whether the `HOOKS_EMPTY_ITERATIONS_COUNT` value maintained in the state
-    object's values by some other complementary hook(s) reached the state object's
-    config setting `HOOKS_EMPTY_ITERATIONS_MAX_COUNT`.
+    check whether the number of iterations (ocurred in a row) which finished with the
+    empty verified elements (most often as a result of
+    `pre_candidates-candidates-selected-verified` function chain) reached the state
+    object's config setting stored under the `HOOKS_EMPTY_ITERATIONS_MAX_COUNT` key
+    constant. The number of the consecutive empty verified elements iterations is
+    expected to be found under the `HOOKS_EMPTY_ITERATIONS_COUNT` key constant and is
+    usually maintained in the state object's values by some other complementary hook(s).
 
     Args:
         state: State object that holds a computation's state.
