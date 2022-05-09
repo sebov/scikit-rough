ed6ebd7fac9ccf0fce1fbda38b87397f5540521d
sebov <12091011+sebov@users.noreply.github.com>
2022-05-10 01:29:23 +0200

    Add returning representatives from choose objects

--- tests/test_instances.py
+++ tests/test_instances.py
@@ -6,42 +6,62 @@ from skrough.structs.group_index import GroupIndex
 
 
 @pytest.mark.parametrize(
-    "group_index,dec_values,permutation,expected",
+    "group_index,dec_values,objs_permutation,expected_all,expected_representatives",
     [
         (
             [0, 1, 2, 1, 1, 1, 2, 2, 0, 0],
             [0, 0, 0, 1, 0, 0, 1, 1, 1, 0],
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             [0, 1, 2, 4, 5, 9],
+            [0, 1, 2],
         ),
         (
             [0, 1, 2, 1, 1, 1, 2, 2, 0, 0],
             [0, 0, 0, 1, 0, 0, 1, 1, 1, 0],
             [0, 3, 2, 1, 4, 5, 6, 7, 8, 9],
             [0, 2, 3, 9],
+            [0, 2, 3],
         ),
         (
             [0, 1, 2, 1, 1, 1, 2, 2, 0, 0],
             [0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
             [0, 3, 2, 1, 4, 5, 6, 7, 8, 9],
             [0, 1, 2, 3, 4, 6, 7],
+            [0, 2, 3],
         ),
     ],
 )
-def test_draw_objects(group_index, dec_values, permutation, expected):
+def test_draw_objects(
+    group_index, dec_values, objs_permutation, expected_all, expected_representatives
+):
     group_index = GroupIndex.create_from_index(np.asarray(group_index))
     dec_values = np.asarray(dec_values)
     dec_values_count = len(np.unique(dec_values))
-    permutation = np.asarray(permutation)
-    expected = np.asarray(expected)
-    result = choose_objects(group_index, dec_values, dec_values_count, permutation)
-    assert all([np.array_equal(result, expected)])
+    objs_permutation = np.asarray(objs_permutation)
+    expected_all = np.asarray(expected_all)
+    expected_representatives = np.asarray(expected_representatives)
+    result_all = choose_objects(
+        group_index,
+        dec_values,
+        dec_values_count,
+        objs_permutation,
+    )
+    assert all([np.array_equal(result_all, expected_all)])
+    result_representatives = choose_objects(
+        group_index,
+        dec_values,
+        dec_values_count,
+        objs_permutation,
+        return_representatives_only=True,
+    )
+    assert all([np.array_equal(result_representatives, expected_representatives)])
 
 
 @pytest.mark.parametrize(
-    "group_index,dec_values,expected",
+    "group_index,dec_values,expected_all,expected_representatives",
     [
         (
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -50,16 +70,32 @@ def test_draw_objects(group_index, dec_values, permutation, expected):
             [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            None,
         ),
     ],
 )
-def test_draw_objects_random(group_index, dec_values, expected):
+def test_draw_objects_random(
+    group_index, dec_values, expected_all, expected_representatives
+):
     group_index = GroupIndex.create_from_index(np.asarray(group_index))
     dec_values = np.asarray(dec_values)
     dec_values_count = len(np.unique(dec_values))
-    expected = np.asarray(expected)
-    result = choose_objects(group_index, dec_values, dec_values_count, objs=None)
-    assert all([np.array_equal(result, expected)])
+    expected_all = np.asarray(expected_all)
+    result_all = choose_objects(
+        group_index,
+        dec_values,
+        dec_values_count,
+    )
+    assert all([np.array_equal(result_all, expected_all)])
+    if expected_representatives is not None:
+        expected_representatives = np.asarray(expected_representatives)
+        result_representatives = choose_objects(
+            group_index,
+            dec_values,
+            dec_values_count,
+            return_representatives_only=True,
+        )
+        assert all([np.array_equal(result_representatives, expected_representatives)])
 
 
 def test_draw_objects_random_2():
@@ -68,7 +104,7 @@ def test_draw_objects_random_2():
     )
     dec_values = np.asarray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     dec_values_count = len(np.unique(dec_values))
-    result = choose_objects(group_index, dec_values, dec_values_count, objs=None)
+    result = choose_objects(group_index, dec_values, dec_values_count)
     assert len(result) == 2
     assert result[0] < 5
     assert 5 <= result[1] < 10
