9a4b1edb2c28e796d39056ea05baa01582c7f035
sebov <12091011+sebov@users.noreply.github.com>
2022-05-22 22:02:14 +0200

    Add homogeneity and heterogeneity tools + tests

--- src/skrough/homogeneity.py
+++ src/skrough/homogeneity.py
@@ -1,6 +1,11 @@
+from typing import Sequence, Tuple
+
 import numba
 import numpy as np
 import numpy.typing as npt
+
+from skrough.distributions import get_dec_distribution
+from skrough.structs.group_index import GroupIndex
 
 
 @numba.njit
@@ -11,9 +16,7 @@ def get_homogeneity(
     Compute decision homogeneity within groups of objects
     """
     if len(distribution.shape) != 2:
-        raise ValueError(
-            "%s - input distribution should be 2d", get_homogeneity.__name__
-        )
+        raise ValueError("get_homogeneity - input distribution should be 2d")
     ngroup, ndec = distribution.shape
     result: npt.NDArray[np.int64] = np.ones(ngroup, dtype=np.int64)
     for i in numba.prange(ngroup):
@@ -54,3 +57,72 @@ def get_heterogeneity(
         if non_zero_values > 1:
             result[i] = heterogeneity_value
     return result
+
+
+@numba.njit
+def _replace_decisions_in_groups(
+    group_ids: np.ndarray,
+    y: np.ndarray,
+    y_count: int,
+    group_decisions: np.ndarray,
+) -> np.ndarray:
+    result = np.empty_like(y)
+    for i in numba.prange(len(y)):
+        if group_decisions[group_ids[i]] == 0:
+            # ``0`` is reserved for non-heterogenous groups, so we preserve the original
+            # decision
+            result[i] = y[i]
+        else:
+            # values > ``0`` represents heterogenous groups, so we set new decisions for
+            # objects belonging to those groups based on ``heterogeneity_values``; but
+            # the new decision values need to be numbered accordingly, i.e., the values
+            # need to be shifted behind the original range of decisions
+            # ``0..(y_count-1)``
+            result[i] = y_count - 1 + group_decisions[group_ids[i]]
+    return result
+
+
+def replace_heterogeneous_decisions(
+    x: np.ndarray,
+    x_counts: np.ndarray,
+    y: np.ndarray,
+    y_count: int,
+    attrs: Sequence[int],
+    distinguish_generalized_decisions: bool = False,
+) -> Tuple[np.ndarray, int]:
+    if len(x) == 0:
+        y, y_count
+
+    group_index = GroupIndex.create_from_data(x, x_counts, attrs)
+    dec_distribution = get_dec_distribution(group_index, y, y_count)
+    if distinguish_generalized_decisions:
+        heterogeneity = get_heterogeneity(dec_distribution)
+    else:
+        heterogeneity = 1 - get_homogeneity(dec_distribution)
+
+    # values ``0`` (if present) mean non-heterogenous groups, i.e., homogenous groups
+    # values > ``0`` (if present) mean heterogenous groups
+    # np.unique returns sorted unique elements
+    heterogeneity_unique_values, heterogeneity_compacted = np.unique(
+        heterogeneity, return_inverse=True
+    )
+    # let's compute the number of heterogenous groups
+    heterogenous_groups_count = len(heterogeneity_unique_values)
+    if heterogeneity_unique_values[0] == 0:
+        # if the value ``0`` is there, we need to adjust, i.e.,
+        # decrease heterogenous_group_count by 1
+        heterogenous_groups_count -= 1
+    else:
+        # otherwise, we need to adjust ``heterogeneity_compacted``, as ``0`` now
+        # represents actual heterogenous group but we want to keep ``0`` reserved for
+        # non-heterogenous ones
+        heterogeneity_compacted += 1
+
+    result = _replace_decisions_in_groups(
+        group_ids=group_index.index,
+        y=y,
+        y_count=y_count,
+        group_decisions=heterogeneity_compacted,
+    )
+
+    return result, (y_count + heterogenous_groups_count)
--- tests/test_homogeneity.py
+++ tests/test_homogeneity.py
@@ -3,17 +3,227 @@
 import numpy as np
 import pytest
 
-from skrough.homogeneity import get_homogeneity
+from skrough.dataprep import prepare_factorized_values, prepare_factorized_x
+from skrough.homogeneity import (
+    get_heterogeneity,
+    get_homogeneity,
+    replace_heterogeneous_decisions,
+)
 
 
 @pytest.mark.parametrize(
-    "distribution,expected",
+    "distribution, expected",
     [
-        ([[2, 0], [0, 0], [1, 1]], [True, True, False]),
-        ([[1, 1], [3, 2], [2, 2]], [False, False, False]),
+        ([[2, 0], [0, 0], [1, 1]], [1, 1, 0]),
+        ([[1, 1], [3, 2], [2, 2]], [0, 0, 0]),
+        (
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 2, 0],
+                [0, 0, 1],
+                [0, 1, 1],
+                [1, 0, 1],
+                [1, 1, 0],
+                [1, 1, 1],
+                [0, 2, 2],
+                [3, 0, 2],
+                [6, 1, 0],
+                [3, 1, 2],
+            ],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        ),
     ],
 )
-def test_compute_homogeneity(distribution, expected):
+def test_get_homogeneity(distribution, expected):
     distribution = np.asarray(distribution)
     result = get_homogeneity(distribution)
     assert all([np.array_equal(result, expected)])
+
+
+@pytest.mark.parametrize(
+    "distribution, expected",
+    [
+        ([[2, 0], [0, 0], [1, 1]], [0, 0, 3]),
+        ([[1, 1], [3, 2], [2, 2]], [3, 3, 3]),
+        (
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 2, 0],
+                [0, 0, 1],
+                [0, 1, 1],
+                [1, 0, 1],
+                [1, 1, 0],
+                [1, 1, 1],
+                [0, 2, 2],
+                [3, 0, 2],
+                [6, 1, 0],
+                [3, 1, 2],
+            ],
+            [0, 0, 0, 0, 3, 5, 6, 7, 3, 5, 6, 7],
+        ),
+    ],
+)
+def test_get_heterogeneity(distribution, expected):
+    distribution = np.asarray(distribution)
+    result = get_heterogeneity(distribution)
+    assert all([np.array_equal(result, expected)])
+
+
+def process_replace_heterogenous_decisions(data, dec, attrs, distinguish):
+    x, x_counts = prepare_factorized_x(data)
+    y, y_count = prepare_factorized_values(dec)
+    result = replace_heterogeneous_decisions(
+        x,
+        x_counts,
+        y,
+        y_count,
+        attrs,
+        distinguish_generalized_decisions=distinguish,
+    )
+    return result
+
+
+replace_heterogenous_decisions_data = np.array(
+    [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 2, 1],
+        [1, 2, 2],
+        [1, 2, 0],
+        [1, 2, 0],
+    ]
+)
+
+
+@pytest.mark.parametrize(
+    "data, dec, attrs, expected_y, expected_y_count",
+    [
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            [0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            2,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 0, 0, 1, 2, 2],
+            [0],
+            [0, 0, 0, 0, 0, 0, 0, 3, 3, 3],
+            4,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+            [0],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            3,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 1, 0, 0, 2, 0, 1, 2, 3],
+            [1],
+            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            5,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 1, 1, 2, 0, 0, 1, 2],
+            [0, 1],
+            [0, 0, 0, 3, 3, 3, 0, 3, 3, 3],
+            4,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 1, 0, 1, 1, 1, 1, 0, 2, 3],
+            [0, 1, 2],
+            [4, 4, 0, 1, 1, 1, 1, 0, 4, 4],
+            5,
+        ),
+    ],
+)
+def test_replace_heterogenous_decisions_no_distinguish(
+    data,
+    dec,
+    attrs,
+    expected_y,
+    expected_y_count,
+):
+    new_y, new_y_count = process_replace_heterogenous_decisions(
+        data,
+        dec,
+        attrs,
+        distinguish=False,
+    )
+    assert new_y_count == expected_y_count
+    assert np.array_equal(new_y, np.asarray(expected_y))
+
+
+@pytest.mark.parametrize(
+    "data, dec, attrs, expected_y, expected_y_count",
+    [
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            [0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+            2,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 0, 0, 1, 2, 2],
+            [0],
+            [0, 0, 0, 0, 0, 0, 0, 3, 3, 3],
+            4,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+            [0],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            3,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 1, 0, 0, 2, 0, 1, 2, 3],
+            [1],
+            [5, 5, 5, 4, 4, 4, 6, 6, 6, 6],
+            7,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 0, 0, 1, 1, 2, 2, 0, 1, 2],
+            [0, 1],
+            [0, 0, 0, 3, 3, 3, 2, 4, 4, 4],
+            5,
+        ),
+        (
+            replace_heterogenous_decisions_data,
+            [0, 1, 2, 0, 1, 1, 3, 1, 1, 2],
+            [0, 1, 2],
+            [5, 5, 2, 5, 5, 5, 3, 1, 4, 4],
+            6,
+        ),
+    ],
+)
+def test_replace_heterogenous_decisions_distinguish(
+    data,
+    dec,
+    attrs,
+    expected_y,
+    expected_y_count,
+):
+    new_y, new_y_count = process_replace_heterogenous_decisions(
+        data,
+        dec,
+        attrs,
+        distinguish=True,
+    )
+    assert new_y_count == expected_y_count
+    assert np.array_equal(new_y, np.asarray(expected_y))
