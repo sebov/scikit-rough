186677d40b6b1dc05841a290e3d5f43398b90c0a
sebov <12091011+sebov@users.noreply.github.com>
2022-05-27 16:41:08 +0200

    Refactor process multi stage

--- src/skrough/algorithms/meta/processing.py
+++ src/skrough/algorithms/meta/processing.py
@@ -47,3 +47,57 @@ def process_multi_stage(
 
     result = prepare_result_hook(state)
     return result
+
+
+# @define
+# class ProcessingMultiStage:
+#     init_state_fun: rght.UpdateStateFunction
+#     init_fun: rght.UpdateStateFunction
+#     stages: Sequence[ProcessingStage]
+#     finalize_fun: rght.UpdateStateFunction
+#     prepare_result_hook: rght.PrepareResultHook
+
+#     @classmethod
+#     @log_start_end(logger)
+#     def from_hooks(
+#         cls,
+#         init_state_hooks: rght.OptionalOneOrSequence[rght.UpdateStateHook],
+#         init_hooks: rght.OptionalOneOrSequence[rght.UpdateStateHook],
+#         process_stages: rght.OptionalOneOrSequence[ProcessingStage],
+#         finalize_hooks: rght.OptionalOneOrSequence[rght.UpdateStateHook],
+#         prepare_result_hook: rght.PrepareResultHook,
+#     ):
+#         return cls(
+#             init_state_fun=aggregate_update_state_hooks(init_state_hooks),
+#             init_fun=aggregate_update_state_hooks(init_hooks),
+#             stages=normalize_hook_sequence(process_stages, optional=True) or [],
+#             finalize_fun=aggregate_update_state_hooks(finalize_hooks),
+#             prepare_result_hook=prepare_result_hook,
+#         )
+
+#     @log_start_end(logger)
+#     def __call__(
+#         self,
+#         state: Optional[ProcessingState],
+#         input: StateInput,
+#         config: StateConfig,
+#         seed: rght.Seed = None,
+#     ) -> None:
+#         logger.debug("Create state object")
+#         rng = np.random.default_rng(seed)
+#         state = ProcessingState(
+#             rng=rng,
+#             config=config,
+#             input=input,
+#         )
+
+#         logger.debug("Run init hooks")
+#         self.init_fun(state)
+
+#         logger.debug("Run stages sequentially")
+#         for i, stage in enumerate(self.stages):
+#             logger.debug("Run stage %d", 0)
+#             stage(state)
+
+#         logger.debug("Run finalize hooks")
+#         self.finalize_fun(state)
