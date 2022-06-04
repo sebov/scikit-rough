0331fe6bfaeda30d7cf40fc03b93081956156ac5
sebov <12091011+sebov@users.noreply.github.com>
2022-06-04 13:44:19 +0200

    Refactor dec distribution test

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -1,5 +1,3 @@
-"""Test reducts"""
-
 import numpy as np
 import pytest
 
@@ -8,27 +6,20 @@ from skrough.structs.group_index import GroupIndex
 
 
 @pytest.mark.parametrize(
-    "group_index,factorized_dec_values,dec_values_count_distinct,expected",
+    "group_index, y, y_count, expected",
     [
-        ([0, 0, 0, 0], [0, 0, 0, 1], 2, ([[3, 1]], [4])),
-        ([0, 0, 1, 1], [0, 0, 0, 1], 2, ([[2, 0], [1, 1]], [2, 2])),
-        ([0, 0, 1, 1], [0, 1, 2, 3], 4, ([[1, 1, 0, 0], [0, 0, 1, 1]], [2, 2])),
+        ([0, 0, 0, 0], [0, 0, 0, 1], 2, [[3, 1]]),
+        ([0, 0, 1, 1], [0, 0, 0, 1], 2, [[2, 0], [1, 1]]),
+        ([0, 0, 1, 1], [0, 1, 2, 3], 4, [[1, 1, 0, 0], [0, 0, 1, 1]]),
     ],
 )
-def test_compute_dec_distribution(
-    group_index, factorized_dec_values, dec_values_count_distinct, expected
-):
+def test_get_dec_distribution(group_index, y, y_count, expected):
     group_index = GroupIndex.create_from_index(group_index)
-    factorized_dec_values = np.asarray(factorized_dec_values)
-    expected_dec_distribution, expected_counts = expected
-    expected_dec_distribution = np.asarray(expected_dec_distribution)
-    # TODO: do we need expected_counts?
-    expected_counts = np.asarray(expected_counts)
-    result_dec_distribution = get_dec_distribution(
-        group_index, factorized_dec_values, dec_values_count_distinct
-    )
+    y = np.asarray(y)
+    expected = np.asarray(expected)
+    dec_distribution = get_dec_distribution(group_index, y, y_count)
     assert all(
         [
-            np.array_equal(result_dec_distribution, expected_dec_distribution),
+            np.array_equal(dec_distribution, expected),
         ]
     )
