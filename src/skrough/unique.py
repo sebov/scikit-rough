be3e14a71614c8996318db0c897cc555512d7947
sebov <12091011+sebov@users.noreply.github.com>
2022-06-28 00:44:16 +0200

    Update unique utils docs and add tests

--- src/skrough/unique.py
+++ src/skrough/unique.py
@@ -4,25 +4,32 @@ import numpy as np
 def get_uniques_index(values: np.ndarray) -> np.ndarray:
     """Get positions of first occurrences of unique values.
 
-    Get positions in the input array for which the given unique values appeared
-    for the first time. Unique values are considered in the ascending order of their
-    values, i.e., from the lowest to the highest unique values occurring in the input
-    array, positions of their first occurrence is returned.
+    Get positions/indices for which unique values in the input array appear for the
+    first time. The indices are reported in the order corresponding to the ascending
+    order of unique values, i.e., the first index indicates the first occurrence of the
+    lowest unique value, the second index indicates the first occurrence of the second
+    lowest unique value, etc.
 
     Args:
         values: Input array.
 
     Returns:
-        The positions in the input array for which given values appeared for
-        the first time.
+        The positions/indices in the input array for which unique values (reported in
+        ascending order) appear for the first time.
 
     Examples:
         >>> get_uniques_index(np.array([1, 2, 3]))
-        [0, 1, 2]
+        array([0, 1, 2])
         >>> get_uniques_index(np.array([3, 2, 1]))
-        [2, 1, 0]
+        array([2, 1, 0])
         >>> get_uniques_index(np.array([1, 1, 1]))
-        [0]
+        array([0])
+        >>> get_uniques_index(np.array([1, 1, 2, 1]))
+        array([0, 2])
+        >>> get_uniques_index(np.array([2, 2, 1, 2]))
+        array([2, 0])
+        >>> get_uniques_index(np.array([]))
+        array([])
     """
     _, idx = np.unique(values, return_index=True)
     return idx
