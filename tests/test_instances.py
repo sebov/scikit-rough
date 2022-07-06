c673247ec876999fbba3439e914b9660fc14aa80
sebov <12091011+sebov@users.noreply.github.com>
2022-07-06 18:36:13 +0200

    Add choose objects tests

--- tests/test_instances.py
+++ tests/test_instances.py
@@ -29,6 +29,13 @@ from skrough.structs.group_index import GroupIndex
             [0, 1, 2, 3, 4, 6, 7],
             [0, 2, 3],
         ),
+        (
+            [],
+            [],
+            [],
+            [],
+            [],
+        ),
     ],
 )
 def test_choose_objects(
@@ -72,12 +79,12 @@ def test_choose_objects(
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             None,
         ),
-        # (
-        #     [],
-        #     [],
-        #     [],
-        #     [],
-        # ),
+        (
+            [],
+            [],
+            [],
+            [],
+        ),
     ],
 )
 def test_choose_objects_random(
