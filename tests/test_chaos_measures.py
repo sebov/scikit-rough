d90f4f1a0bc3558db44994ad785745bcff247a27
sebov <12091011+sebov@users.noreply.github.com>
2022-07-15 09:56:29 +0200

    Add conflicts number chaos measure tests

--- tests/test_chaos_measures.py
+++ tests/test_chaos_measures.py
@@ -4,70 +4,122 @@ import numpy as np
 import pytest
 
 import skrough.typing as rght
-from skrough.chaos_measures.chaos_measures import entropy, gini_impurity
+from skrough.chaos_measures.chaos_measures import (
+    conflicts_number,
+    entropy,
+    gini_impurity,
+)
+
+
+def gini_impurity_alternative_impl(
+    distribution: np.ndarray,
+) -> float:
+    all_sum = distribution.sum()
+    if all_sum == 0:
+        return 0
+    result = 0.0
+    for i in range(distribution.shape[0]):
+        row_sum = distribution[i, :].sum()
+        if row_sum == 0:
+            continue
+        row_result = 0.0
+        for j in range(distribution.shape[1]):
+            value = distribution[i, j]
+            prob = value / row_sum
+            row_result += prob * (1 - prob)
+        result += (row_sum / all_sum) * row_result
+    return result
+
+
+def entropy_alternative_impl(
+    distribution: np.ndarray,
+) -> float:
+    all_sum = distribution.sum()
+    if all_sum == 0:
+        return 0
+    result = 0.0
+    for i in range(distribution.shape[0]):
+        row_sum = distribution[i, :].sum()
+        if row_sum == 0:
+            continue
+        row_result = 0.0
+        for j in range(distribution.shape[1]):
+            value = distribution[i, j]
+            if value > 0:
+                prob = value / row_sum
+                row_result -= prob * np.log2(prob)
+        result += (row_sum / all_sum) * row_result
+    return result
+
+
+def conflicts_number_alternative_impl(
+    distribution: np.ndarray,
+) -> float:
+    all_sum = distribution.sum()
+    if all_sum == 0:
+        return 0
+    result = 0.0
+    for i in range(distribution.shape[0]):
+        row_sum = distribution[i, :].sum()
+        if row_sum == 0:
+            continue
+        row_result = 0.0
+        for j in range(distribution.shape[1]):
+            value = distribution[i, j]
+            row_result += value * (row_sum - value)
+        result += row_result
+    return result / 2
 
 
 def run_compare_measure(
     distribution,
-    expected: rght.ChaosMeasureReturnType,
     measure: rght.ChaosMeasure,
+    measure_alternative_impl,
 ):
     distribution = np.asarray(distribution)
     n_objs = distribution.sum()
     result = measure(distribution, n_objs)
+    expected = measure_alternative_impl(distribution)
     assert np.allclose(result, expected)
 
 
 @pytest.mark.parametrize(
-    "distribution,expected",
+    "chaos_measure, chaos_measure_alternative_impl",
     [
-        ([[4]], 0),
-        ([[1, 1]], 2 / 4),
-        ([[1, 1, 1]], 6 / 9),
-        ([[4, 3, 3]], 66 / 100),
-        ([[3, 1]], 6 / 16),
-        ([[2, 0], [1, 1]], (0 / 4) * (2 / 4) + (2 / 4) * (2 / 4)),
-        ([[1, 1, 0, 0], [0, 0, 1, 1]], (2 / 4) * (2 / 4) + (2 / 4) * (2 / 4)),
-        ([[1, 1, 1, 1], [0, 0, 1, 1]], (12 / 16) * (4 / 6) + (2 / 4) * (2 / 6)),
-        ([[0, 0], [1, 1]], 0 * (0 / 2) + (2 / 4) * (2 / 2)),  # NOSONAR
-        (
-            [[0, 1], [5, 1], [3, 5]],
-            (0 / 1) * (0 / 15) + (10 / 36) * (6 / 15) + (30 / 64) * (8 / 15),
-        ),
-        ([[1, 1]] * 10, 2 / 4),
-        ([[9999, 1]], (10000 * 10000 - 9999 * 9999 - 1 * 1) / (10000 * 10000)),
+        (gini_impurity, gini_impurity_alternative_impl),
+        (entropy, entropy_alternative_impl),
+        (conflicts_number, conflicts_number_alternative_impl),
     ],
 )
-def test_compute_gini_impurity(distribution, expected: rght.ChaosMeasureReturnType):
-    run_compare_measure(distribution, expected, gini_impurity)
-
-
 @pytest.mark.parametrize(
-    "distribution,expected",
+    "distribution",
     [
-        (
-            [[4]],
-            0,
-        ),
-        (
-            [[1, 1]],
-            1,
-        ),
-        (
-            [
-                [0, 2],
-                [1, 1],
-            ],
-            0.5,
-        ),
-        (
-            [
-                [0, 0],
-                [1, 1],
-            ],
-            1,
-        ),
+        [[0]],
+        [[0]] * 10,
+        [[4]],
+        [[0, 0]],
+        [[0, 0]] * 10,
+        [[1, 1]],
+        [[1, 1, 1]],
+        [[4, 3, 3]],
+        [[3, 1]],
+        [[2, 0], [1, 1]],
+        [[1, 1, 0, 0], [0, 0, 1, 1]],
+        [[1, 1, 1, 1], [0, 0, 1, 1]],
+        [[0, 0], [1, 2]],
+        [[0, 2], [1, 1]],
+        [[0, 1], [5, 1], [3, 5]],
+        [[1, 1]] * 10,
+        [[9999, 1]],
+        [[9999, 1], [1, 1]],
+        [[9999, 1], [1, 9999]],
+        [[9999, 1], [0, 0]],
+        [[9999, 1]] * 10,
     ],
 )
-def test_compute_entropy(distribution, expected: rght.ChaosMeasureReturnType):
-    run_compare_measure(distribution, expected, entropy)
+def test_chaos_maesure(
+    chaos_measure,
+    chaos_measure_alternative_impl,
+    distribution,
+):
+    run_compare_measure(distribution, chaos_measure, chaos_measure_alternative_impl)
