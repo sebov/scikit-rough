96f48e9421e28b364c0f607d42b98f97c5b7d985
sebov <12091011+sebov@users.noreply.github.com>
2022-07-16 22:32:59 +0200

    Add prepare_factorized_values tests

--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import pytest
 
 import skrough as rgh
+from skrough.dataprep import prepare_factorized_values
 
 
 def test_prepare_factorized_data(
@@ -13,6 +15,34 @@ def test_prepare_factorized_data(
     )
     assert np.array_equal(pd.DataFrame(x).nunique().to_numpy(), x_counts)
     assert pd.Series(y).nunique() == y_count
+
+
+@pytest.mark.parametrize(
+    "values, expected, expected_count",
+    [
+        ([], [], 0),
+        ([0], [0], 1),
+        ([2], [0], 1),
+        ([-3], [0], 1),
+        ([0, 1, 2], [0, 1, 2], 3),
+        ([1, 2, 10], [0, 1, 2], 3),
+        ([-1, -2, -3], [0, 1, 2], 3),
+        ([1, 4, -10], [0, 1, 2], 3),
+        ([1, 1, -10, 0], [0, 0, 1, 2], 3),
+        ([1, 1, 2, 3, 2, 10, 1], [0, 0, 1, 2, 1, 3, 0], 4),
+        ([1, 1, 1, 1], [0, 0, 0, 0], 1),
+        ([-1, 1, -1, 1], [0, 1, 0, 1], 2),
+    ],
+)
+def test_prepare_factorized_values(values, expected, expected_count):
+    values = np.asarray(values)
+    expected = np.asarray(expected)
+    result, result_count = prepare_factorized_values(values)
+    assert result_count == expected_count
+    assert np.array_equal(result, expected)
+
+
+# TODO: prepare_factorized_x - when data is empty, np.empty(shape=(0,0)/(0,1)/(1,0))
 
 
 def test_add_shadow_attrs(
@@ -47,6 +77,3 @@ def test_add_shadow_attrs(
             orig_values.sort_values(),
             shadow_shadow_values.sort_values(),
         )
-
-
-# TODO: prepare_factorized_x - when data is empty, np.empty(shape=(0,0)/(0,1)/(1,0))
