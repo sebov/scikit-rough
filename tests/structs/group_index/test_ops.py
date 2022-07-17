95805ccbb4dae93808741086ec0754857b305ea5
sebov <12091011+sebov@users.noreply.github.com>
2022-07-17 23:36:40 +0200

    Resolve TODO for adding group index and values length match tests

--- tests/structs/group_index/test_ops.py
+++ tests/structs/group_index/test_ops.py
@@ -1,8 +1,53 @@
-import pandas as pd
+import numpy as np
 import pytest
 
+from skrough.dataprep import prepare_factorized_vector
 from skrough.structs.group_index import GroupIndex
 from tests.structs.group_index.helpers import _assert_group_index
+
+
+@pytest.mark.parametrize(
+    "group_index, values",
+    [
+        (GroupIndex.create_empty(), []),
+        (GroupIndex.create_uniform(0), []),
+        (GroupIndex.create_uniform(1), [0]),
+        (GroupIndex.create_uniform(3), [0, 1, 0]),
+        (GroupIndex.create_uniform(3), [0, 1, 2]),
+        (GroupIndex.create_from_index([]), []),
+        (GroupIndex.create_from_index([1]), [2]),
+        (GroupIndex.create_from_index([0, 2]), [0, 0]),
+    ],
+)
+def test_check_values(group_index: GroupIndex, values):
+    group_index._check_values(values)  # pylint: disable=protected-access
+
+
+@pytest.mark.parametrize(
+    "group_index, values",
+    [
+        (GroupIndex.create_empty(), [0]),
+        (GroupIndex.create_empty(), [0, 1]),
+        (GroupIndex.create_uniform(0), [0]),
+        (GroupIndex.create_uniform(0), [1, 0]),
+        (GroupIndex.create_uniform(1), []),
+        (GroupIndex.create_uniform(1), [0, 1]),
+        (GroupIndex.create_uniform(3), []),
+        (GroupIndex.create_uniform(3), [0]),
+        (GroupIndex.create_uniform(3), [0, 1]),
+        (GroupIndex.create_uniform(3), [0, 1, 0, 1]),
+        (GroupIndex.create_from_index([]), [0]),
+        (GroupIndex.create_from_index([]), [1, 0]),
+        (GroupIndex.create_from_index([1]), []),
+        (GroupIndex.create_from_index([1]), [1, 2]),
+        (GroupIndex.create_from_index([0, 2]), []),
+        (GroupIndex.create_from_index([0, 2]), [0]),
+        (GroupIndex.create_from_index([0, 2]), [1, 0, 0]),
+    ],
+)
+def test_check_values_mismatch(group_index: GroupIndex, values):
+    with pytest.raises(ValueError, match="length does not match the group index"):
+        group_index._check_values(values)  # pylint: disable=protected-access
 
 
 @pytest.mark.parametrize(
@@ -47,13 +92,43 @@ from tests.structs.group_index.helpers import _assert_group_index
 )
 def test_split(input_index, values, expected_index, expected_n_groups, compress):
     group_index = GroupIndex.create_from_index(input_index)
-    factorized_values, uniques = pd.factorize(values)
+    values, values_count = prepare_factorized_vector(np.asarray(values))
     result = group_index.split(
-        factorized_values,
-        len(uniques),
+        values,
+        values_count,
         compress=compress,
     )
     _assert_group_index(result, expected_index, expected_n_groups)
+
+
+@pytest.mark.parametrize(
+    "input_index, values",
+    [
+        ([0, 0], []),
+        ([0, 0], [0]),
+        ([0, 0], [0, 0, 0]),
+        ([0, 0, 0, 0], []),
+        ([0, 0, 0, 0], [0]),
+        ([0, 0, 0, 0], [0, 0, 1]),
+        ([0, 0, 0, 0], [0, 0, 0, 42, 1]),
+        ([0, 1, 0, 1], []),
+        ([0, 1, 0, 1], [0]),
+        ([0, 1, 0, 1], [0, 0, 1]),
+        ([0, 1, 0, 1], [0, 0, 1, 1, 0]),
+        ([0, 2, 0, 3], []),
+        ([0, 2, 0, 3], [1]),
+        ([0, 2, 0, 3], [1, 1]),
+        ([0, 2, 0, 3], [0, 1, 2, 1, 3]),
+    ],
+)
+def test_split_mismatch(input_index, values):
+    group_index = GroupIndex.create_from_index(input_index)
+    values, values_count = prepare_factorized_vector(np.asarray(values))
+    with pytest.raises(ValueError, match="length does not match the group index"):
+        group_index.split(
+            values,
+            values_count,
+        )
 
 
 @pytest.mark.parametrize(
