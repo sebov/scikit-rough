078e9e71b1d8a8ffda27c20aaa04ad01e89e3347
sebov <12091011+sebov@users.noreply.github.com>
2022-07-23 00:54:53 +0200

    Add test for inner_stop_hooks

---
+++ tests/algorithms/hooks/test_inner_stop_hooks.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+from skrough.algorithms.hooks.inner_stop_hooks import inner_stop_hook_empty
+from skrough.structs.state import ProcessingState
+
+
+def dummy_processing_fun(_: ProcessingState):
+    pass
+
+
+@pytest.mark.parametrize(
+    "elements, expected",
+    [
+        ([], True),
+        (np.zeros(shape=0), True),
+        ([0], False),
+        ([1], False),
+        ([1, 10, 0], False),
+    ],
+)
+def test_inner_stop_hook_empty(elements, expected):
+    state = ProcessingState.create_from_optional(
+        rng=np.random.default_rng(),
+        processing_fun=dummy_processing_fun,
+    )
+    assert inner_stop_hook_empty(state, elements=elements) == expected
