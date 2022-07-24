f8f53169ae11d476675d97649d704b78ecba8125
sebov <12091011+sebov@users.noreply.github.com>
2022-07-24 22:47:25 +0200

    Add candidates hooks tests - random choice

--- src/skrough/algorithms/hooks/candidates_hooks.py
+++ src/skrough/algorithms/hooks/candidates_hooks.py
@@ -1,7 +1,7 @@
 import logging
 
 import skrough.typing as rght
-from skrough.algorithms.hooks.names import HOOKS_GROW_CANDIDATES_MAX_COUNT
+from skrough.algorithms.hooks.names import HOOKS_CANDIDATES_MAX_COUNT
 from skrough.logs import log_start_end
 from skrough.structs.state import ProcessingState
 
@@ -9,18 +9,18 @@ logger = logging.getLogger(__name__)
 
 
 @log_start_end(logger)
-def candidates_hooks_grow_attrs_random(
+def candidates_hook_random_choice(
     state: ProcessingState,
     elements: rght.Elements,
 ) -> rght.Elements:
-    candidate_attrs_count = state.config.get(HOOKS_GROW_CANDIDATES_MAX_COUNT)
-    if candidate_attrs_count is None:
-        candidates = elements
-    else:
-        candidates = state.rng.choice(
-            elements,
-            min(len(elements), candidate_attrs_count),
-            replace=False,
-        )
+    candidates_count = state.config.get(HOOKS_CANDIDATES_MAX_COUNT)
+    if candidates_count is None:
+        candidates_count = len(elements)
+    candidates_attrs_count = min(len(elements), candidates_count)
+    candidates = state.rng.choice(
+        elements,
+        size=candidates_attrs_count,
+        replace=False,
+    )
     logger.debug("candidates = %s", candidates)
     return candidates
---
+++ tests/algorithms/hooks/helpers.py
@@ -0,0 +1,5 @@
+from skrough.structs.state import ProcessingState
+
+
+def dummy_processing_fun(_: ProcessingState):
+    """Do nothing function of ProcessingFunction type."""
