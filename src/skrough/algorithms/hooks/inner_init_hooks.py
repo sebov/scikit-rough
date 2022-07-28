d74eeaabcae67e31328fd6f05eac0643d0ac29d0
sebov <12091011+sebov@users.noreply.github.com>
2022-07-29 00:20:16 +0200

    Add inner init hooks tests

--- src/skrough/algorithms/hooks/inner_init_hooks.py
+++ src/skrough/algorithms/hooks/inner_init_hooks.py
@@ -1,7 +1,7 @@
 import logging
 
 import skrough.typing as rght
-from skrough.algorithms.hooks.names import HOOKS_EMPTY_ITERATIONS_COUNT
+from skrough.algorithms.hooks.names import HOOKS_CONSECUTIVE_EMPTY_ITERATIONS_COUNT
 from skrough.logs import log_start_end
 from skrough.structs.state import ProcessingState
 
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 @log_start_end(logger)
-def inner_init_hook_note_empty(
+def inner_init_hook_consecutive_empty_iterations_count(
     state: ProcessingState,
     elements: rght.Elements,
 ) -> rght.Elements:
     if len(elements) == 0:
-        value = state.values.get(HOOKS_EMPTY_ITERATIONS_COUNT, 0) + 1
+        value = state.values.get(HOOKS_CONSECUTIVE_EMPTY_ITERATIONS_COUNT, 0) + 1
     else:
         value = 0
-    state.values[HOOKS_EMPTY_ITERATIONS_COUNT] = value
+    state.values[HOOKS_CONSECUTIVE_EMPTY_ITERATIONS_COUNT] = value
     return elements
