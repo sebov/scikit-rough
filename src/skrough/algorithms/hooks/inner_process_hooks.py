a5c8ebee21f61855ee1f3c21c396fb67ed1211e4
sebov <12091011+sebov@users.noreply.github.com>
2022-07-30 00:05:51 +0200

    Add inner process hooks tests + refactor

--- src/skrough/algorithms/hooks/inner_process_hooks.py
+++ src/skrough/algorithms/hooks/inner_process_hooks.py
@@ -1,7 +1,5 @@
 import logging
 
-import numpy as np
-
 import skrough.typing as rght
 from skrough.algorithms.hooks.names import (
     HOOKS_DATA_X,
@@ -10,22 +8,24 @@ from skrough.algorithms.hooks.names import (
     HOOKS_RESULT_ATTRS,
 )
 from skrough.logs import log_start_end
+from skrough.structs.group_index import GroupIndex
 from skrough.structs.state import ProcessingState
 
 logger = logging.getLogger(__name__)
 
 
 @log_start_end(logger)
-def inner_process_hook_add_attr(
+def inner_process_hook_add_first_attr(
     state: ProcessingState,
     elements: rght.Elements,
 ) -> rght.Elements:
     if len(elements) > 0:
-        attr = np.take(elements, 0)
-        elements = np.delete(elements, 0)
+        attr = elements[0]
+        elements = elements[1:]
         state.values[HOOKS_RESULT_ATTRS].append(attr)
-        state.values[HOOKS_GROUP_INDEX] = state.values[HOOKS_GROUP_INDEX].split(
-            state.values[HOOKS_DATA_X][:, attr],
-            state.values[HOOKS_DATA_X_COUNTS][attr],
+        group_index: GroupIndex = state.values[HOOKS_GROUP_INDEX]
+        state.values[HOOKS_GROUP_INDEX] = group_index.split(
+            values=state.values[HOOKS_DATA_X][:, attr],
+            values_count=state.values[HOOKS_DATA_X_COUNTS][attr],
         )
     return elements
