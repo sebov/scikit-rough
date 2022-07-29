977d681dea32af89361f7eaf9e6e73c3bb58ecee
sebov <12091011+sebov@users.noreply.github.com>
2022-07-30 00:04:57 +0200

    Remove commented out code - process_multi_stage

--- src/skrough/algorithms/meta/processing.py
+++ src/skrough/algorithms/meta/processing.py
@@ -16,42 +16,6 @@ from skrough.structs.state import ProcessingState, StateConfig, StateInputData
 logger = logging.getLogger(__name__)
 
 
-# @log_start_end(logger)
-# def process_multi_stage(
-#     input: StateInput,
-#     config: StateConfig,
-#     init_hooks: Optional[rght.OneOrSequence[rght.UpdateStateHook]],
-#     process_stages: rght.OneOrSequence[ProcessingStage],
-#     finalize_hooks: Optional[rght.OneOrSequence[rght.UpdateStateHook]],
-#     prepare_result_fun: rght.PrepareResultFunction,
-#     seed: rght.Seed = None,
-# ):
-#     init_fun = aggregate_update_state_hooks(init_hooks)
-#     process_stages = normalize_hook_sequence(process_stages, optional=False)
-#     finalize_fun = aggregate_update_state_hooks(finalize_hooks)
-
-#     logger.debug("Create state object")
-#     rng = np.random.default_rng(seed)
-#     state = ProcessingState(
-#         rng=rng,
-#         config=config,
-#         input_data=input,
-#         processing_fun=lambda x: None,  # dummy function
-#     )
-
-#     logger.debug("Run grow_shrink init_hooks")
-#     init_fun(state)
-
-#     for stage in process_stages:
-#         stage(state)
-
-#     logger.debug("Run grow_shrink finalize_hooks")
-#     finalize_fun(state)
-
-#     result = prepare_result_fun(state)
-#     return result
-
-
 @define
 class ProcessingMultiStage:
     init_multi_stage_fun: rght.UpdateStateFunction
