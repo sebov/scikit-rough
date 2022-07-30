f739f429fc3416bcff07810af1de5d46f61be7d2
sebov <12091011+sebov@users.noreply.github.com>
2022-07-30 09:24:13 +0200

    Add pre candidates hooks tests + refactor

---
+++ tests/algorithms/hooks/test_pre_candidates_hooks.py
@@ -0,0 +1,39 @@
+from contextlib import nullcontext as does_not_raise
+
+import numpy as np
+import pytest
+
+from skrough.algorithms.exceptions import LoopBreak
+from skrough.algorithms.hooks.names import HOOKS_DATA_X, HOOKS_RESULT_ATTRS
+from skrough.algorithms.hooks.pre_candidates_hooks import (
+    pre_candidates_hook_remaining_attrs,
+)
+from skrough.structs.state import ProcessingState
+
+
+@pytest.mark.parametrize(
+    "x, result_attrs, exception_raise",
+    [
+        (np.eye(0), [], pytest.raises(LoopBreak, match="No remaining attrs")),
+        (np.eye(5), [], does_not_raise()),
+        (np.eye(5), [0, 1, 2], does_not_raise()),
+        (
+            np.eye(5),
+            [0, 0, 0, 1, 2, 3, 4, 0],
+            pytest.raises(LoopBreak, match="No remaining attrs"),
+        ),
+    ],
+)
+def test_pre_candidates_hook_remaining_attrs(
+    x,
+    result_attrs,
+    exception_raise,
+    state_fixture: ProcessingState,
+):
+    x = np.asarray(x)
+    state_fixture.values[HOOKS_DATA_X] = x
+    state_fixture.values[HOOKS_RESULT_ATTRS] = result_attrs
+    with exception_raise:
+        result_pre_candidates = pre_candidates_hook_remaining_attrs(state_fixture)
+        expected_pre_candidates = set(range(x.shape[1])) - set(result_attrs)
+        assert set(result_pre_candidates) == expected_pre_candidates
