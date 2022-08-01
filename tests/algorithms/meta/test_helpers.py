6542175a63679ce7eba582ba691d7bf23cf9984b
sebov <12091011+sebov@users.noreply.github.com>
2022-08-02 00:09:56 +0200

    Add meta helpers tests - normalize_hook_sequence

--- tests/algorithms/meta/test_helpers.py
+++ tests/algorithms/meta/test_helpers.py
@@ -5,7 +5,7 @@ import pytest
 
 from skrough.algorithms.meta.helpers import normalize_hook_sequence
 
-mock = MagicMock()
+norm_mock = MagicMock()
 
 
 @pytest.mark.parametrize(
@@ -13,34 +13,34 @@ mock = MagicMock()
     [
         (None, False, None, pytest.raises(ValueError, match="should not be empty")),
         (None, True, [], does_not_raise()),
-        (mock, False, [mock], does_not_raise()),
-        (mock, True, [mock], does_not_raise()),
-        (mock.another, False, [mock.another], does_not_raise()),
-        (mock.another, True, [mock.another], does_not_raise()),
-        ([mock.other], False, [mock.other], does_not_raise()),
-        ([mock.other], True, [mock.other], does_not_raise()),
+        (norm_mock, False, [norm_mock], does_not_raise()),
+        (norm_mock, True, [norm_mock], does_not_raise()),
+        (norm_mock.another, False, [norm_mock.another], does_not_raise()),
+        (norm_mock.another, True, [norm_mock.another], does_not_raise()),
+        ([norm_mock.other], False, [norm_mock.other], does_not_raise()),
+        ([norm_mock.other], True, [norm_mock.other], does_not_raise()),
         (
-            [mock.a1, mock.a2, mock.a3],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3],
             False,
-            [mock.a1, mock.a2, mock.a3],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3],
             does_not_raise(),
         ),
         (
-            [mock.a1, mock.a2, mock.a3],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3],
             True,
-            [mock.a1, mock.a2, mock.a3],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3],
             does_not_raise(),
         ),
         (
-            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3, norm_mock.a4, norm_mock.a5],
             False,
-            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3, norm_mock.a4, norm_mock.a5],
             does_not_raise(),
         ),
         (
-            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3, norm_mock.a4, norm_mock.a5],
             True,
-            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            [norm_mock.a1, norm_mock.a2, norm_mock.a3, norm_mock.a4, norm_mock.a5],
             does_not_raise(),
         ),
     ],
