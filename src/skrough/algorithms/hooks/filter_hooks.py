47d822824fb4e75d3def7e62287c756661eaac52
sebov <12091011+sebov@users.noreply.github.com>
2022-08-01 16:01:25 +0200

    Refactor check if attr better than shuffled function

--- src/skrough/algorithms/hooks/filter_hooks.py
+++ src/skrough/algorithms/hooks/filter_hooks.py
@@ -6,15 +6,14 @@ import skrough.typing as rght
 from skrough.algorithms.hooks.names import (
     HOOKS_GROW_POST_SELECT_ATTRS_DAAR_ALLOWED_RANDOMNESS,
     HOOKS_GROW_POST_SELECT_ATTRS_DAAR_N_OF_PROBES,
-    HOOKS_GROW_POST_SELECT_ATTRS_DAAR_SMOOTHING_PARAMETER,
     VALUES_GROUP_INDEX,
     VALUES_X,
     VALUES_X_COUNTS,
     VALUES_Y,
     VALUES_Y_COUNT,
 )
+from skrough.attrs_checks import check_if_attr_better_than_shuffled
 from skrough.logs import log_start_end
-from skrough.structs.group_index import GroupIndex
 from skrough.structs.state import ProcessingState
 
 logger = logging.getLogger(__name__)
@@ -24,57 +23,12 @@ DEFAULT_DAAR_SMOOTHING_PARAMETER = 1
 
 
 @log_start_end(logger)
-def _check_if_better_than_shuffled(
-    group_index: GroupIndex,
-    attr_values: np.ndarray,
-    attr_count: int,
-    y: np.ndarray,
-    y_count: int,
-    n_of_probes: int,
-    smoothing_parameter: float,
-    allowed_randomness: float,
-    chaos_fun: rght.ChaosMeasure,
-    rng: np.random.Generator,
-) -> bool:
-    attr_chaos_score = group_index.get_chaos_score_after_split(
-        attr_values,
-        attr_count,
-        y,
-        y_count,
-        chaos_fun,
-    )
-    attr_is_better_count = 0
-    for _ in range(n_of_probes):
-        attr_values_shuffled = rng.permutation(attr_values)
-        shuffled_chaos_score = group_index.get_chaos_score_after_split(
-            attr_values_shuffled,
-            attr_count,
-            y,
-            y_count,
-            chaos_fun,
-        )
-        attr_is_better_count += int(attr_chaos_score < shuffled_chaos_score)
-
-    smoothing_dims = 2  # binomial distribution, i.e., better/worse
-    attr_probe_score = (attr_is_better_count + smoothing_parameter) / (
-        n_of_probes + smoothing_parameter * smoothing_dims
-    )
-    logger.debug("attr_probe_score == %f", attr_probe_score)
-    return attr_probe_score >= (1 - allowed_randomness)
-
-
-@log_start_end(logger)
 def grow_verify_attrs_daar(
     state: ProcessingState,
     input_attrs: rght.Elements,
 ) -> rght.Elements:
     daar_n_of_probes = state.config[HOOKS_GROW_POST_SELECT_ATTRS_DAAR_N_OF_PROBES]
     logger.debug("Param daar_n_of_probes == %d", daar_n_of_probes)
-    daar_smoothing_parameter = state.config.get(
-        HOOKS_GROW_POST_SELECT_ATTRS_DAAR_SMOOTHING_PARAMETER,
-        DEFAULT_DAAR_SMOOTHING_PARAMETER,
-    )
-    logger.debug("Param daar_smoothing_parameter == %f", daar_smoothing_parameter)
     daar_allowed_randomness = state.config[
         HOOKS_GROW_POST_SELECT_ATTRS_DAAR_ALLOWED_RANDOMNESS
     ]
@@ -85,17 +39,16 @@ def grow_verify_attrs_daar(
     result = []
     for input_attr in input_attrs:
         logger.debug("Check if attr <%d> is better than shuffled", input_attr)
-        if _check_if_better_than_shuffled(
-            state.values[VALUES_GROUP_INDEX],
-            state.values[VALUES_X][:, input_attr],
-            x_counts[input_attr],
-            state.values[VALUES_Y],
-            y_count,
-            daar_n_of_probes,
-            daar_smoothing_parameter,
-            daar_allowed_randomness,
-            chaos_fun,
-            state.rng,
+        if check_if_attr_better_than_shuffled(
+            group_index=state.values[VALUES_GROUP_INDEX],
+            attr_values=state.values[VALUES_X][:, input_attr],
+            attr_values_count=x_counts[input_attr],
+            values=state.values[VALUES_Y],
+            values_count=y_count,
+            n_of_probes=daar_n_of_probes,
+            allowed_randomness=daar_allowed_randomness,
+            chaos_fun=chaos_fun,
+            rng=state.rng,
         ):
             logger.debug(
                 "Attr <%d> is better than shuffled with respect to allowed_randomness",
---
+++ src/skrough/attrs_checks.py
@@ -0,0 +1,52 @@
+import logging
+
+import numpy as np
+
+import skrough.typing as rght
+from skrough.logs import log_start_end
+from skrough.structs.group_index import GroupIndex
+
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_SMOOTHING_PARAMETER = 1
+
+
+@log_start_end(logger)
+def check_if_attr_better_than_shuffled(
+    group_index: GroupIndex,
+    attr_values: np.ndarray,
+    attr_values_count: int,
+    values: np.ndarray,
+    values_count: int,
+    n_of_probes: int,
+    allowed_randomness: float,
+    chaos_fun: rght.ChaosMeasure,
+    rng: np.random.Generator,
+    smoothing_parameter: float = DEFAULT_SMOOTHING_PARAMETER,
+) -> bool:
+    attr_chaos_score = group_index.get_chaos_score_after_split(
+        attr_values,
+        attr_values_count,
+        values,
+        values_count,
+        chaos_fun,
+    )
+    attr_is_better_count = 0
+    for _ in range(n_of_probes):
+        attr_values_shuffled = rng.permutation(attr_values)
+        shuffled_chaos_score = group_index.get_chaos_score_after_split(
+            attr_values_shuffled,
+            attr_values_count,
+            values,
+            values_count,
+            chaos_fun,
+        )
+        attr_is_better_count += int(attr_chaos_score < shuffled_chaos_score)
+
+    smoothing_dims = 2  # binomial distribution, i.e., better/worse
+    attr_probe_score = (attr_is_better_count + smoothing_parameter) / (
+        n_of_probes + smoothing_parameter * smoothing_dims
+    )
+    logger.debug("attr_probe_score == %f", attr_probe_score)
+    return attr_probe_score >= (1 - allowed_randomness)
