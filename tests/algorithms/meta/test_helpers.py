33c7b0c577c3f09d3c54832e95f76b4091c1fb96
sebov <12091011+sebov@users.noreply.github.com>
2022-08-01 23:29:46 +0200

    Add meta helpers tests - normalize_hook_sequence

--- tests/algorithms/meta/test_helpers.py
+++ tests/algorithms/meta/test_helpers.py
@@ -1,11 +1,11 @@
 from contextlib import nullcontext as does_not_raise
+from unittest.mock import MagicMock
 
 import pytest
 
 from skrough.algorithms.meta.helpers import normalize_hook_sequence
 
-# a collection of dummy functions
-dummy = [lambda x=k: x for k in range(10)]
+mock = MagicMock()
 
 
 @pytest.mark.parametrize(
@@ -13,16 +13,36 @@ dummy = [lambda x=k: x for k in range(10)]
     [
         (None, False, None, pytest.raises(ValueError, match="should not be empty")),
         (None, True, [], does_not_raise()),
-        (dummy[0], False, [dummy[0]], does_not_raise()),
-        (dummy[0], True, [dummy[0]], does_not_raise()),
-        (dummy[1], False, [dummy[1]], does_not_raise()),
-        (dummy[1], True, [dummy[1]], does_not_raise()),
-        ([dummy[5]], False, [dummy[5]], does_not_raise()),
-        ([dummy[5]], True, [dummy[5]], does_not_raise()),
-        (dummy[:5], False, dummy[:5], does_not_raise()),
-        (dummy[:5], True, dummy[:5], does_not_raise()),
-        (dummy, False, dummy, does_not_raise()),
-        (dummy, True, dummy, does_not_raise()),
+        (mock, False, [mock], does_not_raise()),
+        (mock, True, [mock], does_not_raise()),
+        (mock.another, False, [mock.another], does_not_raise()),
+        (mock.another, True, [mock.another], does_not_raise()),
+        ([mock.other], False, [mock.other], does_not_raise()),
+        ([mock.other], True, [mock.other], does_not_raise()),
+        (
+            [mock.a1, mock.a2, mock.a3],
+            False,
+            [mock.a1, mock.a2, mock.a3],
+            does_not_raise(),
+        ),
+        (
+            [mock.a1, mock.a2, mock.a3],
+            True,
+            [mock.a1, mock.a2, mock.a3],
+            does_not_raise(),
+        ),
+        (
+            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            False,
+            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            does_not_raise(),
+        ),
+        (
+            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            True,
+            [mock.a1, mock.a2, mock.a3, mock.a4, mock.a5],
+            does_not_raise(),
+        ),
     ],
 )
 def test_normalize_hook_sequence(hooks, optional, expected, exception_raise):
