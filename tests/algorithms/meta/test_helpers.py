0fd9f47d680be4f3eb08420f46ef16e9b4194dc5
sebov <12091011+sebov@users.noreply.github.com>
2022-08-02 11:55:57 +0200

    Add meta helpers tests - aggregate_update_state_hooks

--- tests/algorithms/meta/test_helpers.py
+++ tests/algorithms/meta/test_helpers.py
@@ -7,6 +7,7 @@ from skrough.algorithms.exceptions import LoopBreak
 from skrough.algorithms.meta.helpers import (
     aggregate_any_inner_stop_hooks,
     aggregate_any_stop_hooks,
+    aggregate_update_state_hooks,
     normalize_hook_sequence,
 )
 from skrough.structs.state import ProcessingState
@@ -86,8 +87,8 @@ def test_aggregate_any_stop_hooks(
     mock = MagicMock()
     mock.side_effect = hook_values
     with exception_raise:
-        agg_hook = aggregate_any_stop_hooks([mock for _ in range(len(hook_values))])
-        result = agg_hook(state=state_fixture, raise_loop_break=raise_loop_break)
+        agg_hooks = aggregate_any_stop_hooks([mock for _ in range(len(hook_values))])
+        result = agg_hooks(state=state_fixture, raise_loop_break=raise_loop_break)
         assert result is any(hook_values)
         # call count - it should be lazy and stop on first True
         expected_call_count = (
@@ -109,10 +110,10 @@ def test_aggregate_any_inner_stop_hooks(
     mock = MagicMock()
     mock.side_effect = hook_values
     with exception_raise:
-        agg_hook = aggregate_any_inner_stop_hooks(
+        agg_hooks = aggregate_any_inner_stop_hooks(
             [mock for _ in range(len(hook_values))]
         )
-        result = agg_hook(
+        result = agg_hooks(
             state=state_fixture,
             elements=[],
             raise_loop_break=raise_loop_break,
@@ -123,3 +124,19 @@ def test_aggregate_any_inner_stop_hooks(
             hook_values.index(True) + 1 if True in hook_values else len(hook_values)
         )
         assert mock.call_count == expected_call_count
+
+
+@pytest.mark.parametrize(
+    "hooks_count",
+    [0, 1, 2, 5],
+)
+def test_aggregate_update_state_hooks(
+    hooks_count,
+    state_fixture: ProcessingState,
+):
+    mock = MagicMock()
+    agg_hooks = aggregate_update_state_hooks([mock for _ in range(hooks_count)])
+    agg_hooks(state_fixture)
+    assert mock.call_count == hooks_count
+    for call in mock.call_args_list:
+        assert call.args == (state_fixture,)
