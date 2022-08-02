1be254b229cb3787d36b86571746215deaa03722
sebov <12091011+sebov@users.noreply.github.com>
2022-08-02 11:01:09 +0200

    Add meta helpers tests - aggregate_any_inner_stop_hooks

--- tests/algorithms/meta/test_helpers.py
+++ tests/algorithms/meta/test_helpers.py
@@ -5,6 +5,7 @@ import pytest
 
 from skrough.algorithms.exceptions import LoopBreak
 from skrough.algorithms.meta.helpers import (
+    aggregate_any_inner_stop_hooks,
     aggregate_any_stop_hooks,
     normalize_hook_sequence,
 )
@@ -58,20 +59,23 @@ def test_normalize_hook_sequence(hooks, optional, expected, exception_raise):
         assert result == expected
 
 
+stop_hooks_parametrize = [
+    ([], False, pytest.raises(ValueError, match="should not be empty")),
+    ([], True, pytest.raises(ValueError, match="should not be empty")),
+    ([False], False, does_not_raise()),
+    ([False], True, does_not_raise()),
+    ([True], False, does_not_raise()),
+    ([True], True, pytest.raises(LoopBreak)),
+    ([False, False, True], False, does_not_raise()),
+    ([False, False, True], True, pytest.raises(LoopBreak)),
+    ([True, False, True], False, does_not_raise()),
+    ([True, False, True], True, pytest.raises(LoopBreak)),
+]
+
+
 @pytest.mark.parametrize(
     "hook_values, raise_loop_break, exception_raise",
-    [
-        ([], False, pytest.raises(ValueError, match="should not be empty")),
-        ([], True, pytest.raises(ValueError, match="should not be empty")),
-        ([False], False, does_not_raise()),
-        ([False], True, does_not_raise()),
-        ([True], False, does_not_raise()),
-        ([True], True, pytest.raises(LoopBreak)),
-        ([False, False, True], False, does_not_raise()),
-        ([False, False, True], True, pytest.raises(LoopBreak)),
-        ([True, False, True], False, does_not_raise()),
-        ([True, False, True], True, pytest.raises(LoopBreak)),
-    ],
+    stop_hooks_parametrize,
 )
 def test_aggregate_any_stop_hooks(
     hook_values,
@@ -83,7 +87,36 @@ def test_aggregate_any_stop_hooks(
     mock.side_effect = hook_values
     with exception_raise:
         agg_hook = aggregate_any_stop_hooks([mock for _ in range(len(hook_values))])
-        result = agg_hook(state_fixture, raise_loop_break=raise_loop_break)
+        result = agg_hook(state=state_fixture, raise_loop_break=raise_loop_break)
+        assert result is any(hook_values)
+        # call count - it should be lazy and stop on first True
+        expected_call_count = (
+            hook_values.index(True) + 1 if True in hook_values else len(hook_values)
+        )
+        assert mock.call_count == expected_call_count
+
+
+@pytest.mark.parametrize(
+    "hook_values, raise_loop_break, exception_raise",
+    stop_hooks_parametrize,
+)
+def test_aggregate_any_inner_stop_hooks(
+    hook_values,
+    raise_loop_break,
+    exception_raise,
+    state_fixture: ProcessingState,
+):
+    mock = MagicMock()
+    mock.side_effect = hook_values
+    with exception_raise:
+        agg_hook = aggregate_any_inner_stop_hooks(
+            [mock for _ in range(len(hook_values))]
+        )
+        result = agg_hook(
+            state=state_fixture,
+            elements=[],
+            raise_loop_break=raise_loop_break,
+        )
         assert result is any(hook_values)
         # call count - it should be lazy and stop on first True
         expected_call_count = (
