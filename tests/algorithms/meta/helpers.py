11b270e67b3e309811c090bd65e1abae8db08384
sebov <12091011+sebov@users.noreply.github.com>
2022-09-16 22:46:16 +0200

    Refactor test for  stage describe method

---
+++ tests/algorithms/meta/helpers.py
@@ -0,0 +1,15 @@
+from skrough.algorithms.meta.describe import DescriptionNode
+
+DUMMY_NODE_NAME = "dummy"
+LEAF_VALUE = "value"
+
+
+def get_describe_dict(describe_node: DescriptionNode):
+    """Prepare helper dict structure for better navigation in tests."""
+    result = {}
+    if describe_node.children is not None:
+        for element in describe_node.children:
+            result[element.node_name] = get_describe_dict(element)
+    else:
+        result[LEAF_VALUE] = describe_node
+    return result
