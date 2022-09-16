e1309fdba8bf476ee18df9fef9ab99bf8d01c753
sebov <12091011+sebov@users.noreply.github.com>
2022-09-16 22:46:16 +0200

    Refactor test for  stage describe method

--- tests/algorithms/meta/test_stage.py
+++ tests/algorithms/meta/test_stage.py
@@ -5,6 +5,7 @@ import pytest
 from skrough.algorithms.meta.describe import DescriptionNode
 from skrough.algorithms.meta.stage import Stage
 from skrough.structs.state import ProcessingState
+from tests.algorithms.meta.helpers import DUMMY_NODE_NAME, LEAF_VALUE, get_describe_dict
 
 
 @pytest.mark.parametrize(
@@ -103,21 +104,6 @@ def test_stage_outer_inner_loop_k_m_iters(
     assert inner_init_hook.call_count == outer_iters
     assert inner_process_hook.call_count == outer_iters * inner_iters
     assert finalize_hook.call_count == 1
-
-
-DUMMY_NODE_NAME = "dummy"
-LEAF_VALUE = "value"
-
-
-def get_describe_dict(describe_node: DescriptionNode):
-    """Prepare helper dict structure for better navigation in tests."""
-    result = {}
-    if describe_node.children is not None:
-        for element in describe_node.children:
-            result[element.node_name] = get_describe_dict(element)
-    else:
-        result[LEAF_VALUE] = describe_node
-    return result
 
 
 def test_describe():
