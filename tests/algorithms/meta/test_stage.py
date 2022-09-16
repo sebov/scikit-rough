ff954c71fb18f80fdb7e2d7ab8ad0e9e4da5bc97
sebov <12091011+sebov@users.noreply.github.com>
2022-09-16 22:38:41 +0200

    Add test for  stage describe method

--- tests/algorithms/meta/test_stage.py
+++ tests/algorithms/meta/test_stage.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock
 
 import pytest
 
+from skrough.algorithms.meta.describe import DescriptionNode
 from skrough.algorithms.meta.stage import Stage
 from skrough.structs.state import ProcessingState
 
@@ -102,3 +103,76 @@ def test_stage_outer_inner_loop_k_m_iters(
     assert inner_init_hook.call_count == outer_iters
     assert inner_process_hook.call_count == outer_iters * inner_iters
     assert finalize_hook.call_count == 1
+
+
+DUMMY_NODE_NAME = "dummy"
+LEAF_VALUE = "value"
+
+
+def get_describe_dict(describe_node: DescriptionNode):
+    """Prepare helper dict structure for better navigation in tests."""
+    result = {}
+    if describe_node.children is not None:
+        for element in describe_node.children:
+            result[element.node_name] = get_describe_dict(element)
+    else:
+        result[LEAF_VALUE] = describe_node
+    return result
+
+
+def test_describe():
+    mock = MagicMock()
+    dummy = DescriptionNode(
+        node_name=DUMMY_NODE_NAME,
+        name="name",
+        short_description="short_description",
+        long_description="long_description",
+    )
+    mock.describe.return_value = dummy
+
+    stage = Stage.from_hooks(
+        stop_hooks=mock,
+        init_hooks=mock,
+        pre_candidates_hooks=mock,
+        candidates_hooks=mock,
+        select_hooks=mock,
+        filter_hooks=mock,
+        inner_init_hooks=mock,
+        inner_stop_hooks=mock,
+        inner_process_hooks=mock,
+        finalize_hooks=mock,
+    )
+    result = stage.describe()
+    assert result.name == Stage.__name__
+
+    stage_dict = get_describe_dict(result)
+
+    assert stage_dict["init"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert stage_dict["check_stop"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert (
+        stage_dict["outer_loop"]["pre_candidates"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    )
+    assert stage_dict["outer_loop"]["candidates"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert stage_dict["outer_loop"]["select"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert stage_dict["outer_loop"]["filter"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert stage_dict["outer_loop"]["inner_init"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert (
+        stage_dict["outer_loop"]["inner_loop"]["inner_check_stop"][DUMMY_NODE_NAME][
+            LEAF_VALUE
+        ]
+        == dummy
+    )
+    assert (
+        stage_dict["outer_loop"]["inner_loop"]["inner_process"][DUMMY_NODE_NAME][
+            LEAF_VALUE
+        ]
+        == dummy
+    )
+    assert (
+        stage_dict["outer_loop"]["inner_loop"]["check_stop"][DUMMY_NODE_NAME][
+            LEAF_VALUE
+        ]
+        == dummy
+    )
+    assert stage_dict["outer_loop"]["check_stop"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
+    assert stage_dict["finalize"][DUMMY_NODE_NAME][LEAF_VALUE] == dummy
