17331b977444a33ba2c990bb4265257ca21a1ddb
sebov <12091011+sebov@users.noreply.github.com>
2022-09-29 01:33:48 +0200

    Add attrs_subset docs

--- src/skrough/structs/attrs_subset.py
+++ src/skrough/structs/attrs_subset.py
@@ -1,3 +1,5 @@
+"""Attrs subset structures."""
+
 from typing import List, Union
 
 from attrs import define
@@ -7,10 +9,28 @@ import skrough.typing as rght
 
 @define
 class AttrsSubset:
+    """A class to represent a subset of attributes.
+
+    A class to represent a subset of attributes. They are stored in a form of
+    integer-location based indexing sequence of attributes.
+    """
+
     attrs: List[int]
+    """Subset of attributes."""
 
     @classmethod
     def from_attrs_like(cls, attrs_subset_like: Union["AttrsSubset", rght.AttrsLike]):
+        """Create a new instance.
+
+        Create a new instance using the ``attrs_subset_like`` input value.
+
+        Args:
+            attrs_subset_like: A base value that should be used to create a new
+                instance.
+
+        Returns:
+            A new instance created using the ``attrs_subset_like`` argument.
+        """
         attrs = (
             attrs_subset_like.attrs
             if isinstance(attrs_subset_like, AttrsSubset)
