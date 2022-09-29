b31f666d0b7fcc50b6d03aa7c1f8661d9bc3624f
sebov <12091011+sebov@users.noreply.github.com>
2022-09-29 18:16:29 +0200

    Add objs_attrs_subset docs

--- src/skrough/structs/objs_attrs_subset.py
+++ src/skrough/structs/objs_attrs_subset.py
@@ -1,3 +1,5 @@
+"""Dual objects-attrs subset structures."""
+
 from typing import List
 
 from attrs import define
@@ -7,9 +9,29 @@ import skrough.typing as rght
 
 @define
 class ObjsAttrsSubset:
+    """A class to represent a subset of objects and attributes.
+
+    A class to represent a subset of objects and attributes. They are both stored
+    separately in a form of integer-location based indexing sequence of objects and
+    attributes, respectively.
+    """
+
     objs: List[int]
     attrs: List[int]
 
     @classmethod
     def from_objs_attrs_like(cls, objs_like: rght.ObjsLike, attrs_like: rght.AttrsLike):
+        """Create a new instance.
+
+        Create a new instance using the ``objs_like`` and ``attrs_like`` input values.
+
+        Args:
+            objs_like: A base value that should be used to initialize objects subset
+                when creating a new instance.
+            attrs_like: A base value that should be used to initialize attributes subset
+                when creating a new instance.
+
+        Returns:
+            A new instance created using the ``objs_like`` and ``attrs_like`` arguments.
+        """
         return cls(objs=list(objs_like), attrs=list(attrs_like))
