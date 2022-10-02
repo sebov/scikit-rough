b6248239f7d9a6210eeafe8151df81ac95b7864b
sebov <12091011+sebov@users.noreply.github.com>
2022-10-03 00:32:40 +0200

    Add exceptions docs

--- src/skrough/algorithms/exceptions.py
+++ src/skrough/algorithms/exceptions.py
@@ -1,2 +1,10 @@
+"""Exceptions related to algorithms."""
+
+
 class LoopBreak(Exception):
-    pass
+    """A class used to represent a loop break event.
+
+    An exception class to be used in hook functions (cf.
+    :mod:`~skrough.algorithms.hooks`) to represent loop break event causing an exit from
+    execution of a particular :class:`~skrough.algorithms.meta.stage.Stage`.
+    """
