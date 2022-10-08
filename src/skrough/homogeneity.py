7c2c26a320d830109c6ff5f539174cf5aaee085d
sebov <12091011+sebov@users.noreply.github.com>
2022-10-08 16:47:36 +0200

    Add homogeneity docs

--- src/skrough/homogeneity.py
+++ src/skrough/homogeneity.py
@@ -158,14 +158,14 @@ def get_heterogeneity(
 
 @numba.njit
 def _replace_decisions_in_groups(
-    group_ids: np.ndarray,
+    group_index: np.ndarray,
     y: np.ndarray,
     y_count: int,
     group_decisions: np.ndarray,
 ) -> np.ndarray:
     result = np.empty_like(y)
     for i in numba.prange(len(y)):  # pylint: disable=not-an-iterable
-        if group_decisions[group_ids[i]] == 0:
+        if group_decisions[group_index[i]] == 0:
             # ``0`` is reserved for non-heterogenous groups, so we preserve the original
             # decision
             result[i] = y[i]
@@ -175,11 +175,11 @@ def _replace_decisions_in_groups(
             # the new decision values need to be numbered accordingly, i.e., the values
             # need to be shifted behind the original range of decisions
             # ``0..(y_count-1)``
-            result[i] = y_count - 1 + group_decisions[group_ids[i]]
+            result[i] = y_count - 1 + group_decisions[group_index[i]]
     return result
 
 
-def replace_heterogeneous_decisions(
+def replace_heterogeneous_groups_decisions(
     x: np.ndarray,
     x_counts: np.ndarray,
     y: np.ndarray,
@@ -190,21 +190,84 @@ def replace_heterogeneous_decisions(
     """Return consistent decision values.
 
     Prepare new decision values in a way that makes data consistent (in the meaning of a
-    consistent decision table). The groups, i.e., equivalence classes in the context of
-    the indiscernibility relation, are induced from the given dataset ``x`` and a subset
-    of attributes ``attrs``. Original decisions ``y`` are then processed to prepare new
-    consistent decision values. It is done by preserving decision values for ...
+    consistent decision table). The groups (equivalence classes in the context of the
+    indiscernibility relation) are induced from the given dataset ``x`` and a subset of
+    attributes ``attrs``. Original decisions ``y`` are then processed to prepare new
+    consistent decision values. It is done by preserving decision values for homogenous
+    groups and replacing decisions for objects from heterogenous ones. The
+    ``distinguish_generalized_decisions`` boolean flag can be used to control whether
+    heterogenous groups should be distinguished from each other
+    (:code:`distinguish_generalized_decisions is True`) or treated equally
+    (:code:`distinguish_generalized_decisions is False`). Distinguishing the
+    heterogenous groups means that objects from groups of different characteristics (a
+    different subset of decision values appearing in a group, cf.
+    :func:`~skrough.homogeneity.get_heterogeneity`) are assigned different new decision
+    values. When heterogenous groups are not to be distinguished then objects from all
+    heterogenous groups are assigned the same new decision value.
 
     Args:
-        x: _description_
-        x_counts: _description_
-        y: _description_
-        y_count: _description_
-        attrs: _description_
-        distinguish_generalized_decisions: _description_. Defaults to False.
+        x: Factorized data table representing conditional features/attributes for the
+            objects the computation should be performed on. The values in each column
+            should be given in a form of integer-location based indexing sequence of the
+            factorized conditional attribute values, i.e., 0-based values that index
+            distinct values of the conditional attribute.
+        x_counts: Number of distinct attribute values given for each conditional
+            attribute. The argument is expected to be given as a 1D array.
+        y: Factorized decision values for the objects represented by the input
+            :obj:`x` argument. The values should be given in a form of integer-location
+            based indexing sequence of the factorized decision values, i.e., 0-based
+            values that index distinct decisions.
+        y_count: Number of distinct decision attribute values.
+        attrs: A subset of conditional attributes the check should be performed on.
+            It should be given in a form of a sequence of integer-location based
+            indexing of the selected conditional attributes from ``x``. :obj:`None`
+            value means to use all available conditional attributes. Defaults to
+            :obj:`None`.
+        distinguish_generalized_decisions: A flag to control whether heterogenous groups
+            should be distinguished from each other or not. Defaults to :obj:`False`.
 
     Returns:
-        _description_
+        New decision values returned in a form of 2-element tuple with the following
+        elements
+
+        - factorized decision attribute returned in form of 1d array
+        - decision attribute domain size
+
+        The new decision values together with the input data ``x`` and ``x_counts`` form
+        a consistent decision table.
+
+    Examples:
+        >>> from skrough.dataprep import (
+        ...     prepare_factorized_array,
+        ...     prepare_factorized_vector
+        ... )
+        >>> x, x_counts = prepare_factorized_array(np.asarray([[8, 8, 8],
+        ...                                                    [8, 8, 8],
+        ...                                                    [1, 7, 8],
+        ...                                                    [1, 8, 8],
+        ...                                                    [1, 1, 8],
+        ...                                                    [1, 1, 1]]))
+        >>> y, y_count = prepare_factorized_vector(np.asarray([3, 4, 8, 9, 4, 5]))
+        >>> y, y_count
+        (array([0, 1, 2, 3, 1, 3]), 5)
+        >>> replace_heterogeneous_groups_decisions(
+        ...     x,
+        ...     x_counts,
+        ...     y,
+        ...     y_count,
+        ...     attrs=[0, 1],
+        ...     distinguish_generalized_decisions=False,
+        ... )
+        (array([5, 5, 2, 3, 5, 5]), 6)
+        >>> replace_heterogeneous_groups_decisions(
+        ...     x,
+        ...     x_counts,
+        ...     y,
+        ...     y_count,
+        ...     attrs=[0, 1],
+        ...     distinguish_generalized_decisions=True,
+        ... )
+        (array([6, 6, 2, 3, 5, 5]), 7)
     """
     if len(x) == 0:
         return y, y_count
@@ -218,7 +281,7 @@ def replace_heterogeneous_decisions(
 
     # values ``0`` (if present) mean non-heterogenous groups, i.e., homogenous groups
     # values > ``0`` (if present) mean heterogenous groups
-    # np.unique returns sorted unique elements
+    # get_uniques_and_compacted returns unique elements as sorted (ascending) sequence
     heterogeneity_unique_values, heterogeneity_compacted = get_uniques_and_compacted(
         heterogeneity
     )
@@ -235,7 +298,7 @@ def replace_heterogeneous_decisions(
         heterogeneity_compacted += 1
 
     result = _replace_decisions_in_groups(
-        group_ids=group_index.index,
+        group_index=group_index.index,
         y=y,
         y_count=y_count,
         group_decisions=heterogeneity_compacted,
