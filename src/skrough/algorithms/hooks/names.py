57c486b5a119c0b71ce30246067989e736f5b606
sebov <12091011+sebov@users.noreply.github.com>
2022-10-21 23:48:17 +0200

    Add names docs

--- src/skrough/algorithms/hooks/names.py
+++ src/skrough/algorithms/hooks/names.py
@@ -31,10 +31,15 @@ VALUES_CHAOS_SCORE_APPROX_THRESHOLD = "values_chaos_score_approx_value_threshold
 VALUES_CHAOS_SCORE_BASE = "values_chaos_score_base"
 VALUES_CHAOS_SCORE_TOTAL = "values_chaos_score_total"
 VALUES_CONSECUTIVE_EMPTY_ITERATIONS_COUNT = "values_consecutive_empty_iterations_count"
-VALUES_GROUP_INDEX = "values_group_index"
-VALUES_RESULT_ATTRS = "values_result_attrs"
-VALUES_RESULT_OBJS = "values_result_objs"
 
+VALUES_GROUP_INDEX = "values_group_index"
+"""A key used to reference the current group index."""
+
+VALUES_RESULT_ATTRS = "values_result_attrs"
+"""A key used to reference the result attributes."""
+
+VALUES_RESULT_OBJS = "values_result_objs"
+"""A key used to reference the result objects."""
 
 VALUES_X = "values_x"
 """A key used to reference the factorized data table representing conditional
