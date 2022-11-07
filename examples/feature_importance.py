2a782f580ab4734a065572df647309f23804b9a2
sebov <12091011+sebov@users.noreply.github.com>
2022-11-07 09:38:56 +0100

    Cleanup feature importance example

--- examples/feature_importance.py
+++ examples/feature_importance.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -16,8 +16,6 @@
 # # Feature importance example
 
 # %%
-"""Feature importance example."""
-
 import pprint
 
 import more_itertools
