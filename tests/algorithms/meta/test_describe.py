0101a196d268aa759021389aec75cfe23d86040b
sebov <12091011+sebov@users.noreply.github.com>
2022-11-20 00:49:54 +0100

    Use attrs instead of dataclasses

--- tests/algorithms/meta/test_describe.py
+++ tests/algorithms/meta/test_describe.py
@@ -1,7 +1,7 @@
-from dataclasses import replace
 from textwrap import dedent
 
 import pytest
+from attrs import evolve
 
 from skrough.algorithms.meta.describe import DescriptionNode, describe
 
@@ -207,7 +207,7 @@ def test_describe_list(
         else:
             expected = INT_DESCRIPTION_NODE
         # set expected node_name, i.e., str(position) in children list
-        expected = replace(expected, node_name=str(i))
+        expected = evolve(expected, node_name=str(i))
         assert result.children[i] == expected
 
 
