0fe6b83e14d958f6b69b10ea094416e8f4f93734
sebov <12091011+sebov@users.noreply.github.com>
2022-11-21 00:23:02 +0100

    Add keys tests

---
+++ tests/algorithms/meta/test_get_keys.py
@@ -0,0 +1,115 @@
+from typing import List
+
+import pytest
+
+from skrough.algorithms.meta.describe import (
+    determine_config_keys,
+    determine_input_keys,
+    determine_values_keys,
+)
+
+DETERMINE_FUNCTIONS = {
+    "config": determine_config_keys,
+    "input": determine_input_keys,
+    "values": determine_values_keys,
+}
+
+KEYS_FROM_DOCSTRING = {
+    "config": ["CONFIG_KEY_1", "CONFIG_KEY_2", "CONFIG_KEY_3_3_", "CONFIG___YEK_4__"],
+    "input": ["INPUT_KEY_1", "INPUT_KEY_2", "INPUT_KEY_3_3_", "INPUT___YEK_4__"],
+    "values": ["VALUES_KEY_1", "VALUES_KEY_2", "VALUES_KEY_3_3_", "VALUES___YEK_4__"],
+}
+
+KEYS_FROM_METHODS = {
+    "config": ["CONFIG_KEY_5", "CONFIG_KEY_6", "CONFIG_KEY_7_7_", "CONFIG___YEK_8__"],
+    "input": ["INPUT_KEY_5", "INPUT_KEY_6", "INPUT_KEY_7_7_", "INPUT___YEK_8__"],
+    "values": ["VALUES_KEY_5", "VALUES_KEY_6", "VALUES_KEY_7_7_", "VALUES___YEK_8__"],
+}
+
+
+class ClassNoMethodsAndNoDocstring:
+    pass
+
+
+class ClassNoMethodsAndEmptyDocstring:  # pylint: disable=empty-docstring
+    """"""
+
+
+class ClassNoMethodsAndNoKeysDocstring:
+    """aaa bbb ccc"""
+
+
+class ClassNoMethodsAndDocstring:
+    """
+    CONFIG_KEY_1, `CONFIG_KEY_2`, aaabbccc, ``CONFIG_KEY_3_3_``
+    INPUT_KEY_1, `INPUT_KEY_2`, aaabbccc, ``INPUT_KEY_3_3_``
+    VALUES_KEY_1, `VALUES_KEY_2`, aaabbccc, ``VALUES_KEY_3_3_``
+
+    VALUES___YEK_4__
+
+    INPUT___YEK_4__
+
+    CONFIG___YEK_4__
+    """
+
+
+class ClassMethodsAndNoDocstring:
+    def get_config_keys(self) -> List[str]:
+        return ["CONFIG_KEY_5", "CONFIG_KEY_6", "CONFIG_KEY_7_7_", "CONFIG___YEK_8__"]
+
+    def get_input_keys(self) -> List[str]:
+        return ["INPUT_KEY_5", "INPUT_KEY_6", "INPUT_KEY_7_7_", "INPUT___YEK_8__"]
+
+    def get_values_keys(self) -> List[str]:
+        return ["VALUES_KEY_5", "VALUES_KEY_6", "VALUES_KEY_7_7_", "VALUES___YEK_8__"]
+
+
+class ClassMethodsAndCallable:
+    """
+    CONFIG_KEY_1, `CONFIG_KEY_2`, aaabbccc, ``CONFIG_KEY_3_3_``
+    INPUT_KEY_1, `INPUT_KEY_2`, aaabbccc, ``INPUT_KEY_3_3_``
+    VALUES_KEY_1, `VALUES_KEY_2`, aaabbccc, ``VALUES_KEY_3_3_``
+
+    VALUES___YEK_4__
+
+    INPUT___YEK_4__
+
+    CONFIG___YEK_4__
+    """
+
+    def get_config_keys(self) -> List[str]:
+        return ["CONFIG_KEY_5", "CONFIG_KEY_6", "CONFIG_KEY_7_7_", "CONFIG___YEK_8__"]
+
+    def get_input_keys(self) -> List[str]:
+        return ["INPUT_KEY_5", "INPUT_KEY_6", "INPUT_KEY_7_7_", "INPUT___YEK_8__"]
+
+    def get_values_keys(self) -> List[str]:
+        return ["VALUES_KEY_5", "VALUES_KEY_6", "VALUES_KEY_7_7_", "VALUES___YEK_8__"]
+
+
+@pytest.mark.parametrize(
+    "klass",
+    [
+        ClassNoMethodsAndNoDocstring,
+        ClassNoMethodsAndEmptyDocstring,
+        ClassNoMethodsAndNoKeysDocstring,
+    ],
+)
+def test_no_keys(klass):
+    processing_element = klass()
+    for fun in DETERMINE_FUNCTIONS.values():
+        assert fun(processing_element) == []
+
+
+@pytest.mark.parametrize(
+    "klass, expected_map",
+    [
+        (ClassNoMethodsAndDocstring, KEYS_FROM_DOCSTRING),
+        (ClassMethodsAndNoDocstring, KEYS_FROM_METHODS),
+        (ClassMethodsAndCallable, KEYS_FROM_METHODS),
+    ],
+)
+def test_some_keys(klass, expected_map):
+    processing_element = klass()
+    for key_kind, fun in DETERMINE_FUNCTIONS.items():
+        assert fun(processing_element) == expected_map[key_kind]
