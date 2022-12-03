a3d7a062bceb347abaa34d1fa96f211fb37dfda6
sebov <12091011+sebov@users.noreply.github.com>
2022-12-03 02:00:38 +0100

    Add docs

--- src/skrough/algorithms/key_names.py
+++ src/skrough/algorithms/key_names.py
@@ -19,6 +19,8 @@ INPUT_DATA_Y = "input_data_y"
 
 CONFIG_CANDIDATES_MAX_COUNT = "config_candidates_max_count"
 CONFIG_CHAOS_FUN = "config_chaos_fun"
+"""A key used to reference the chaos measure function to be used."""
+
 CONFIG_CONSECUTIVE_EMPTY_ITERATIONS_MAX_COUNT = (
     "config_consecutive_empty_iterations_max_count"
 )
@@ -29,12 +31,20 @@ CONFIG_SELECT_ATTRS_CHAOS_SCORE_BASED_MAX_COUNT = (
     "config_select_attrs_chaos_score_based_max_count"
 )
 CONFIG_SELECT_RANDOM_MAX_COUNT = "config_select_attrs_random_max_count"
-CONFIG_RESULT_ATTRS_MAX_COUNT = "config_result_attrs_max_count"
+"""A key used to reference the max number of randomly selected candidate elements."""
 
+CONFIG_RESULT_ATTRS_MAX_COUNT = "config_result_attrs_max_count"
+"""A key used to reference the max number of attrs in the results."""
 
 VALUES_CHAOS_SCORE_APPROX_THRESHOLD = "values_chaos_score_approx_value_threshold"
+"""A key used to reference the chaos score approximation threshold."""
+
 VALUES_CHAOS_SCORE_BASE = "values_chaos_score_base"
+"""A key used to reference the chaos score base value."""
+
 VALUES_CHAOS_SCORE_TOTAL = "values_chaos_score_total"
+"""A key used to reference the chaos score total value."""
+
 VALUES_CONSECUTIVE_EMPTY_ITERATIONS_COUNT = "values_consecutive_empty_iterations_count"
 
 VALUES_GROUP_INDEX = "values_group_index"
