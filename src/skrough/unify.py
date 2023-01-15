3a7078a5dad08b4f5a02e9dfff00a1d539869567
sebov <12091011+sebov@users.noreply.github.com>
2023-01-15 22:24:35 +0100

    Add unify_locations docs

--- src/skrough/unify.py
+++ src/skrough/unify.py
@@ -5,17 +5,26 @@ import numpy as np
 import skrough.typing as rght
 
 
-def unify_locations(objs: rght.LocationsLike) -> rght.Locations:
+def unify_locations(items: rght.LocationsLike) -> rght.Locations:
     """Unify locations-like input.
 
     Unify locations-like input, i.e., :class:`numpy.ndarray` or a sequence of
-    integer-location based indexing of objects/attributes/elements to common form
-    of :class:`~skrough.typing.Locations`.
+    integer-location based indexing of objects/attributes/elements to a common form of
+    :class:`~skrough.typing.Locations`.
 
     Args:
-        objs: _description_
+        items: Locations-like input to be unified into a common form of
+        :class:`~skrough.typing.Locations`.
 
     Returns:
-        _description_
+        Unified locations.
+
+    Examples:
+        >>> unify_locations(np.arange(5))
+        array([0, 1, 2, 3, 4])
+        >>> unify_locations([1,2,3])
+        array([1, 2, 3])
+        >>> unify_locations([])
+        array([], dtype=int64)
     """
-    return np.asarray(objs, dtype=np.int64)
+    return np.asarray(items, dtype=np.int64)
