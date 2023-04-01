b8a5ed6d3841e3c1882ee87bc14123711597ef72
sebov <12091011+sebov@users.noreply.github.com>
2023-04-01 10:53:24 +0200

    Add attr reduction stage

--- src/skrough/algorithms/hooks/inner_process_hooks.py
+++ src/skrough/algorithms/hooks/inner_process_hooks.py
@@ -1,6 +1,7 @@
 import logging
 
 import skrough.typing as rght
+from skrough.algorithms.hooks.helpers import check_if_below_approx_value_threshold
 from skrough.algorithms.key_names import (
     VALUES_GROUP_INDEX,
     VALUES_RESULT_ATTRS,
@@ -29,4 +30,21 @@ def inner_process_hook_add_first_attr(
             values_count=state.values[VALUES_X_COUNTS][attr],
             compress=True,
         )
+    return elements
+
+
+def inner_process_hook_discard_first_attr_approx_threshold(
+    state: ProcessingState,
+    elements: rght.Elements,
+) -> rght.Elements:
+    attr = elements[0]
+    elements = elements[1:]
+    attrs_to_try = [a for a in state.values[VALUES_RESULT_ATTRS] if a != attr]
+    group_index = GroupIndex.from_data(
+        x=state.values[VALUES_X],
+        x_counts=state.values[VALUES_X_COUNTS],
+        attrs=attrs_to_try,
+    )
+    if check_if_below_approx_value_threshold(state, group_index):
+        state.values[VALUES_RESULT_ATTRS] = attrs_to_try
     return elements
