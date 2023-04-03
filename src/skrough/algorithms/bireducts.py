5fcc04bab0e6e70d7199301e348d8b7556efd759
sebov <12091011+sebov@users.noreply.github.com>
2023-04-03 07:37:19 +0200

    Remove old greedy bireducts

--- src/skrough/algorithms/bireducts.py
+++ src/skrough/algorithms/bireducts.py
@@ -19,41 +19,6 @@ from skrough.algorithms.reusables.attrs_greedy import greedy_stage
 from skrough.algorithms.reusables.attrs_reduction import reduction_stage
 from skrough.dataprep import prepare_factorized_array, prepare_factorized_vector
 
-# _get_bireduct_greedy_heuristic = processing.ProcessingMultiStage.from_hooks(
-#     init_multi_stage_hooks=[
-#         hooks.init_hooks.init_hook_factorize_data_x_y,
-#         hooks.init_hooks.init_hook_single_group_index,
-#         hooks.init_hooks.init_hook_result_attrs_empty,
-#         hooks.init_hooks.init_hook_approx_threshold,
-#     ],
-#     stages=[greedy_stage, reduction_stage],
-#     finalize_hooks=hooks.finalize_hooks.finalize_hook_choose_objs_randomly,
-#     prepare_result_fun=hooks.prepare_result_hooks.prepare_result_hook_objs_attrs_subset,
-# )
-
-
-# def get_bireduct_greedy_heuristic(
-#     x,
-#     y,
-#     epsilon: float,
-#     candidates_count: Optional[int],
-#     chaos_measure: rght.ChaosMeasure,
-#     seed: rght.Seed = None,
-# ):
-#     return _get_bireduct_greedy_heuristic(
-#         input_data={
-#             INPUT_DATA_X: x,
-#             INPUT_DATA_Y: y,
-#         },
-#         config={
-#             CONFIG_CHAOS_FUN: chaos_measure,
-#             CONFIG_EPSILON: epsilon,
-#             CONFIG_SELECT_ATTRS_CHAOS_SCORE_BASED_MAX_COUNT: 1,
-#             CONFIG_CANDIDATES_SELECT_RANDOM_MAX_COUNT: candidates_count,
-#         },
-#         seed=seed,
-#     )
-
 _get_bireduct_greedy_heuristic = processing.ProcessingMultiStage.from_hooks(
     init_multi_stage_hooks=[
         hooks.init_hooks.init_hook_pass_data,
