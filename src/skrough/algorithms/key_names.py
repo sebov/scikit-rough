9e6ffd4b1ac39e5a3eafe5dea6f1952752089549
sebov <12091011+sebov@users.noreply.github.com>
2025-10-02 16:12:25 +0200

    Cleanup

--- src/skrough/algorithms/key_names.py
+++ src/skrough/algorithms/key_names.py
@@ -1,55 +1,9 @@
-"""Constants holding key names used in
-:class:`~skrough.structs.state.ProcessingState` and its
-:attr:`~skrough.structs.state.ProcessingState.config`."""
-
-# INPUT_DATA_X = "input_data_x"
-# """A key used to reference the input data table representing conditional
-# features/attributes in ProcessingState.input_data."""
-
-# INPUT_DATA_X_COUNTS = "input_data_x_counts"
-# """A key used to reference the input data table domain sizes in
-# ProcessingState.input_data."""
-
-# INPUT_DATA_Y = "input_data_y"
-# """A key used to reference the input decision values in ProcessingState.input_data."""
-
-# INPUT_DATA_Y_COUNT = "input_data_y_count"
-# """A key used to reference the input decision values domain size in
-# ProcessingState.input_data."""
-
-# CONFIG_DISORDER_FUN = "config_disorder_fun"
-# """A key used to reference the disorder measure function to be used."""
-
 # CONFIG_CONSECUTIVE_EMPTY_ITERATIONS_MAX_COUNT = (
 #     "config_consecutive_empty_iterations_max_count"
 # )
-# CONFIG_DAAR_ALLOWED_RANDOMNESS = "config_daar_allowed_randomness"
-# CONFIG_DAAR_FAST = "config_daar_fast"
-# CONFIG_DAAR_PROBES_COUNT = "config_daar_probes_count"
-# CONFIG_DAAR_SMOOTHING_PARAMETER = "config_daar_smoothing"
-# CONFIG_EPSILON = "config_epsilon"
-# CONFIG_SELECT_ATTRS_DISORDER_SCORE_BASED_MAX_COUNT = (
-#     "config_select_attrs_disorder_score_based_max_count"
-# )
-# CONFIG_CANDIDATES_SELECT_RANDOM_MAX_COUNT = "config_candidates_select_random_max_count"
-# """A key used to reference the max number of randomly selected candidate elements."""
 
 # CONFIG_RESULT_ATTRS_MAX_COUNT = "config_result_attrs_max_count"
 # """A key used to reference the max number of attrs in the results."""
-
-# CONFIG_SET_APPROX_THRESHOLD_TO_CURRENT = "config_set_approx_threshold_to_current"
-# """A key used to control the hook that sets approx threshold available under
-# ProcessingState.values['VALUES_DISORDER_SCORE_APPROX_THRESHOLD'] to the value resulting
-# from the current group index and decision values."""
-
-# VALUES_DISORDER_SCORE_APPROX_THRESHOLD = "values_disorder_score_approx_threshold"
-# """A key used to reference the disorder score approximation threshold."""
-
-# VALUES_DISORDER_SCORE_BASE = "values_disorder_score_base"
-# """A key used to reference the disorder score base value."""
-
-# VALUES_DISORDER_SCORE_TOTAL = "values_disorder_score_total"
-# """A key used to reference the disorder score total value."""
 
 # VALUES_CONSECUTIVE_EMPTY_ITERATIONS_COUNT = "values_consecutive_empty_iterations_count"
 
@@ -65,9 +19,3 @@
 # VALUES_X = "values_x"
 # """A key used to reference the factorized data table representing conditional
 # features/attributes."""
-
-# VALUES_X_COUNTS = "values_x_counts"
-# """A key used to reference the factorized data table domain sizes."""
-
-# VALUES_Y = "values_y"
-# """A key used to reference the factorized decision values."""
